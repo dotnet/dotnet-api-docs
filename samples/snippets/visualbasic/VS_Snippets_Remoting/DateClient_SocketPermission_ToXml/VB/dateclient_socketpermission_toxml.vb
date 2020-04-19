240044ed0a2dede3042a940a675098b429ec8f7f
Maira Wenzel <mairaw@microsoft.com>
2020-04-18 19:06:43 -0700

    additional fixes

--- samples/snippets/visualbasic/VS_Snippets_Remoting/DateClient_SocketPermission_ToXml/VB/dateclient_socketpermission_toxml.vb
+++ samples/snippets/visualbasic/VS_Snippets_Remoting/DateClient_SocketPermission_ToXml/VB/dateclient_socketpermission_toxml.vb
@@ -11,7 +11,7 @@
 '  instance of 'SocketPermission' is created with the 'SocketPermission(NetworkAccess,
 '   TransportType, string, int)' constructor. A third 'SocketPermission' object is 
 '  formed from the union of the above two 'SocketPermission' objects with the use of the
-'  'Union' method of 'SocketPermission' class . This 'SocketPermission' object is used by
+'  'Union' method of 'SocketPermission' class. This 'SocketPermission' object is used by
 '  the 'GetDate' method to verify the permissions of the calling method. If the calling 
 '  method has the requisite permissions the 'GetDate' method connects to the 'DateServer'
 '  and returns the current date that the 'DateServer' sends. If any exception occurs
--- samples/snippets/visualbasic/VS_Snippets_Winforms/DataGridColumnStyle_ReadOnlyChanged/VB/datagridcolumnstyle_readonlychanged.vb
+++ samples/snippets/visualbasic/VS_Snippets_Winforms/DataGridColumnStyle_ReadOnlyChanged/VB/datagridcolumnstyle_readonlychanged.vb
@@ -4,7 +4,7 @@
 ' The following example demonstrates 'ReadOnlyChanged' Event of
 ' 'DataGridColumnStyle' class. It adds DataGrid and Button to a form. When
 ' user clicks on button the 'ReadOnly'  property of 'DataGridColumnStyle' is
-' changed . This raises the'ReadOnlyChanged' event which calls the user defined
+' changed. This raises the'ReadOnlyChanged' event which calls the user defined
 ' EventHandler function.
 '
 
