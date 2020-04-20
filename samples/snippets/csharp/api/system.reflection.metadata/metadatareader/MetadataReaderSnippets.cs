6cb6d3267949da8d8c7aa36cc4267bd5e48b7129
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2020-04-20 22:54:15 +0500

    MetadataReader: add remarks and example

---
+++ samples/snippets/csharp/api/system.reflection.metadata/metadatareader/MetadataReaderSnippets.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+
+namespace MetadataReaderSnippets
+{
+    class MetadataReaderSnippets
+    {
+        public static void Run()
+        {
+            //<SnippetMetadataReader>
+            FileStream fs = new FileStream("Example.dll", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+
+            using (fs)
+            {
+                PEReader peReader = new PEReader(fs);
+
+                using (peReader)
+                {
+                    MetadataReader mr = peReader.GetMetadataReader();
+
+                    foreach (TypeDefinitionHandle tdefh in mr.TypeDefinitions)
+                    {
+                        TypeDefinition tdef = mr.GetTypeDefinition(tdefh);
+
+                        string ns = mr.GetString(tdef.Namespace);
+                        string name = mr.GetString(tdef.Name);
+                        Console.WriteLine($"{ns}.{name}");
+                    }
+                }
+            }
+            //</SnippetMetadataReader>
+        }
+    }
+}
