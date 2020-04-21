a963fb0536703a03b9e7184bcd84227906a501fc
Maira Wenzel <mairaw@microsoft.com>
2020-04-20 17:37:10 -0700

    Update networkcredential_username_password_domain.cpp

--- samples/snippets/cpp/VS_Snippets_Remoting/NetworkCredential_UserName_Password_Domain/CPP/networkcredential_username_password_domain.cpp
+++ samples/snippets/cpp/VS_Snippets_Remoting/NetworkCredential_UserName_Password_Domain/CPP/networkcredential_username_password_domain.cpp
@@ -1,11 +1,11 @@
 
 
 // System::Net::NetworkCredential::Username;System::Net::NetworkCredential::Domain;System::Net::NetworkCredential::Password
-/*This program demontrates the 'UserName', 'Domain' and 'Password' properties of 'NetworkCredential' class.
-It takes an URL, username, password and domainname from console. An empty 'NetworkCredential' Object*
-is created. The 'UserName' , 'Password' and 'Domain' porperties of 'NetworkCredential' class are initialised
+/*This program demontrates the 'UserName', 'Domain', and 'Password' properties of the 'NetworkCredential' class.
+It takes an URL, username, password, and domain name from console. An empty 'NetworkCredential' Object*
+is created. The 'UserName', 'Password', and 'Domain' porperties of the 'NetworkCredential' class are initialized
 with the respective values taken from console. Then a 'WebRequest' Object* is created and the 'NetworkCredential'
-Object* is associated with it.A message is displayed onto the console on successful reception of response
+Object* is associated with it. A message is displayed onto the console on successful reception of response
 otherwise an exception is thrown.
 */
 
