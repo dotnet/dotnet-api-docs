0c5e850822430b075fa6bd5a53209398fa91c0f6
Maira Wenzel <mairaw@microsoft.com>
2020-04-20 17:45:13 -0700

    Update source.cs

--- samples/snippets/csharp/VS_Snippets_Misc/system.net.httpwebrequest.addrange/cs/source.cs
+++ samples/snippets/csharp/VS_Snippets_Misc/system.net.httpwebrequest.addrange/cs/source.cs
@@ -1,5 +1,5 @@
 ﻿/*System.Net.HttpWebRequest.AddRange(int,int)
-This program demonstrates 'AddRange(int)' method of 'HttpWebRequest class.
+This program demonstrates the 'AddRange(int)' method of the 'HttpWebRequest class.
 A new 'HttpWebRequest' object is created. The number of characters of the response to be received can be
 restricted by the 'AddRange' method. By calling 'AddRange(50,150)' on the 'HttpWebRequest' object, the content
 of the response page is restricted from the 50th character to 150th charater. The response of the request is
