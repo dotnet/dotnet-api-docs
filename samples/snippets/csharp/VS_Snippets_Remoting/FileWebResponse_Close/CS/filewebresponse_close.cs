e356f42d02c516e5e71e8619445357775847a9e8
Maira Wenzel <mairaw@microsoft.com>
2020-04-20 17:48:35 -0700

    Update filewebresponse_close.cs

--- samples/snippets/csharp/VS_Snippets_Remoting/FileWebResponse_Close/CS/filewebresponse_close.cs
+++ samples/snippets/csharp/VS_Snippets_Remoting/FileWebResponse_Close/CS/filewebresponse_close.cs
@@ -1,7 +1,7 @@
 ﻿// System.Net.FileWebResponse.Close
 
 /*This program demontrates the  'Close' method of 'FileWebResponse' Class.
-It takes an Uri from console and creates a 'FileWebRequest' object for the Uri.It then gets back
+It takes an Uri from console and creates a 'FileWebRequest' object for the Uri. It then gets back
 the response object from the Uri. The response object can be processed as desired. The program then
 closes the response object and releases resources associated with it.*/
 
