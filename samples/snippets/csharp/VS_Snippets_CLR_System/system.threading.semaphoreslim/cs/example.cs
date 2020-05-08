65eca8c64e9e8ea3e0de3dce7be75f51bb1f64b5
Robin Sue <robinsue@live.de>
2020-05-09 00:26:31 +0200

    Format the sample code to make it easier to read (#4208)
    The way of how Task.Run avoided indenting it's content and hiding the closing braces on the last Console.WriteLine line made it really hard to see what's going on here.

--- samples/snippets/csharp/VS_Snippets_CLR_System/system.threading.semaphoreslim/cs/example.cs
+++ samples/snippets/csharp/VS_Snippets_CLR_System/system.threading.semaphoreslim/cs/example.cs
@@ -16,25 +16,27 @@ public class Example
         Console.WriteLine("{0} tasks can enter the semaphore.",
                           semaphore.CurrentCount);
         Task[] tasks = new Task[5];
-        
+
         // Create and start five numbered tasks.
-        for(int i = 0; i <= 4; i++)
+        for (int i = 0; i <= 4; i++)
         {
-            tasks[i] = Task.Run( () => {
-            // Each task begins by requesting the semaphore.
-            Console.WriteLine("Task {0} begins and waits for the semaphore.",
-                              Task.CurrentId);
-            semaphore.Wait();
+            tasks[i] = Task.Run(() =>
+            {
+                // Each task begins by requesting the semaphore.
+                Console.WriteLine("Task {0} begins and waits for the semaphore.",
+                                  Task.CurrentId);
+                semaphore.Wait();
 
-            Interlocked.Add(ref padding, 100);
+                Interlocked.Add(ref padding, 100);
 
-            Console.WriteLine("Task {0} enters the semaphore.", Task.CurrentId);
+                Console.WriteLine("Task {0} enters the semaphore.", Task.CurrentId);
 
-            // The task just sleeps for 1+ seconds.
-            Thread.Sleep(1000 + padding);
+                // The task just sleeps for 1+ seconds.
+                Thread.Sleep(1000 + padding);
 
-            Console.WriteLine("Task {0} releases the semaphore; previous count: {1}.",
-                              Task.CurrentId, semaphore.Release()); } );
+                Console.WriteLine("Task {0} releases the semaphore; previous count: {1}.",
+                                  Task.CurrentId, semaphore.Release());
+            });
         }
 
         // Wait for half a second, to allow all the tasks to start and block.
@@ -47,7 +49,7 @@ public class Example
                           semaphore.CurrentCount);
         // Main thread waits for the tasks to complete.
         Task.WaitAll(tasks);
-        
+
         Console.WriteLine("Main thread exits.");
     }
 }
