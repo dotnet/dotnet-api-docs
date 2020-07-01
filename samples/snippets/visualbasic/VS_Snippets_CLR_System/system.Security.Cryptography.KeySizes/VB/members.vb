e5970ac853a81ceaae26c357880f9a9c0380b0aa
Tom Dykstra <tdykstra@microsoft.com>
2020-07-01 12:39:02 -0700

    fix expected output

--- samples/snippets/visualbasic/VS_Snippets_CLR_System/system.Security.Cryptography.KeySizes/VB/members.vb
+++ samples/snippets/visualbasic/VS_Snippets_CLR_System/system.Security.Cryptography.KeySizes/VB/members.vb
@@ -218,7 +218,7 @@ End Class
 ' Maximum key size bits: 256
 ' Interval between key size bits: 64
 ' 
-' RSACryptoServiceProvider KeySize = 384
+' RSA KeySize = 512
 ' KeySizes retrieved from the
 ' System.Security.Cryptography.RSA object.
 ' Minimum key size bits: 512
