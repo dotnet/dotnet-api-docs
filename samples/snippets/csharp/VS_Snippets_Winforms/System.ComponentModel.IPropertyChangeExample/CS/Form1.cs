e3f88cf6a3ca8965a06d7d51bf1958cd0bbbdd63
Youssef Victor <31348972+Youssef1313@users.noreply.github.com>
2020-08-02 11:09:06 +0200

    Use string instead of String

--- samples/snippets/csharp/VS_Snippets_Winforms/System.ComponentModel.IPropertyChangeExample/CS/Form1.cs
+++ samples/snippets/csharp/VS_Snippets_Winforms/System.ComponentModel.IPropertyChangeExample/CS/Form1.cs
@@ -89,7 +89,7 @@ namespace TestNotifyPropertyChangedCS
         // This method is called by the Set accessor of each property.
         // The CallerMemberName attribute that is applied to the optional propertyName
         // parameter causes the property name of the caller to be substituted as an argument.
-        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
         {
             if (PropertyChanged != null)
             {
