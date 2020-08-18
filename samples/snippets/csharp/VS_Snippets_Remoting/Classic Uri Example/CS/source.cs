b5caf91589bf69872c6fb72b2869d79d31dde82d
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2020-08-18 07:42:16 -0700

    clean up spacing

--- samples/snippets/csharp/VS_Snippets_Remoting/Classic Uri Example/CS/source.cs
+++ samples/snippets/csharp/VS_Snippets_Remoting/Classic Uri Example/CS/source.cs
@@ -5,13 +5,12 @@ using System.Windows.Forms;
 
 public class Form1: Form
 {
- protected void Method()
- {
-// <Snippet1>
-Uri siteUri = new Uri("http://www.contoso.com/");
+  protected void Method()
+  {
+    // <Snippet1>
+    Uri siteUri = new Uri("http://www.contoso.com/");
 
-WebRequest wr = WebRequest.Create(siteUri);
-
-// </Snippet1>
- }
+    WebRequest wr = WebRequest.Create(siteUri);
+    // </Snippet1>
+  }
 }
