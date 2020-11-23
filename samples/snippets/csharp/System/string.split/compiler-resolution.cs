73e106e071747457441fa2bb38c51feaeabe171e
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2020-11-23 15:32:55 -0800

    Improve String.Split article (#5065)

---
+++ samples/snippets/csharp/System/string.split/compiler-resolution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Split
+{
+    class CompilerResolution
+    {
+        public static void Main6()
+        {
+            // <Snippet1>
+            string value = "This is a short string.";
+            char delimiter = 's';
+            string[] substrings = value.Split(delimiter);
+            foreach (var substring in substrings)
+                Console.WriteLine(substring);
+
+            // The example displays the following output:
+            //     Thi
+            //      i
+            //      a
+            //     hort
+            //     tring.
+            // </Snippet1>
+        }
+    }
+}
---
+++ samples/snippets/visualbasic/System/string.split/Program.vb
@@ -0,0 +1,9 @@
+﻿Imports System
+
+Module Program
+    Sub Main(args As String())
+        Intro1()
+        Intro2()
+        Intro3()
+    End Sub
+End Module
