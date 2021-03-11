14589c270cfe912da6b963fc4d276b77e590d301
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2021-03-11 16:44:55 +0500

    Add project

---
+++ samples/snippets/csharp/api/system.reflection.metadata.ecma335/metadatabuilder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MetadataBuilderSnippets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MetadataBuilderSnippets.Run();
+            Console.WriteLine("End");
+            Console.ReadKey();
+        }
+    }
+}
