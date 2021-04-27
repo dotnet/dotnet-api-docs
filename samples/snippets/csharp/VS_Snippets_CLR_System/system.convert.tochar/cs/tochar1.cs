460a09d75cb26755bcca30a28bdd8af7c66a7ce2
Maira Wenzel <mairaw@microsoft.com>
2021-04-26 17:35:28 -0700

    fix snippet tag

--- samples/snippets/csharp/VS_Snippets_CLR_System/system.convert.tochar/cs/tochar1.cs
+++ samples/snippets/csharp/VS_Snippets_CLR_System/system.convert.tochar/cs/tochar1.cs
@@ -89,18 +89,18 @@ public class Example
                               number);
          }
       }
+      //       -1 is outside the range of the Char data type.
+      //       0 converts to ' '.
+      //       40 converts to '('.
+      //       160 converts to ' '.
+      //       255 converts to 'ÿ'.
+      //       1028 converts to '?'.
+      //       2011 converts to '?'.
+      //       30001 converts to '?'.
+      //       207154 is outside the range of the Char data type.
+      //       2147483647 is outside the range of the Char data type.
+      // </Snippet3>
    }
-   //       -1 is outside the range of the Char data type.
-   //       0 converts to ' '.
-   //       40 converts to '('.
-   //       160 converts to ' '.
-   //       255 converts to 'ÿ'.
-   //       1028 converts to '?'.
-   //       2011 converts to '?'.
-   //       30001 converts to '?'.
-   //       207154 is outside the range of the Char data type.
-   //       2147483647 is outside the range of the Char data type.
-   // </Snippet3>
 
    private static void ConvertSByte()
    {
