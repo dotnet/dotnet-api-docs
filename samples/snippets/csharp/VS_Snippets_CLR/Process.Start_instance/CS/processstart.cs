228bfd6c8307d01078c9bb1133c14c5ba485d34f
GitHubPang <61439577+GitHubPang@users.noreply.github.com>
2021-05-13 08:03:12 +0800

    Fix typo in comment in processstart.* - "is is".

--- samples/snippets/csharp/VS_Snippets_CLR/Process.Start_instance/CS/processstart.cs
+++ samples/snippets/csharp/VS_Snippets_CLR/Process.Start_instance/CS/processstart.cs
@@ -19,7 +19,7 @@ namespace MyProcessSample
                     myProcess.StartInfo.CreateNoWindow = true;
                     myProcess.Start();
                     // This code assumes the process you are starting will terminate itself.
-                    // Given that is is started without a window so you cannot terminate it
+                    // Given that it is started without a window so you cannot terminate it
                     // on the desktop, it must terminate itself or you can do it programmatically
                     // from this application using the Kill method.
                 }
