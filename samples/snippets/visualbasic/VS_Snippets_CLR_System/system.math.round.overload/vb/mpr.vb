0280489b078648f4f8b7211a9fc9fc61036f2a86
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2021-06-01 17:22:19 -0700

    update vb examples to match

--- samples/snippets/visualbasic/VS_Snippets_CLR_System/system.math.round.overload/vb/mpr.vb
+++ samples/snippets/visualbasic/VS_Snippets_CLR_System/system.math.round.overload/vb/mpr.vb
@@ -1,42 +1,49 @@
-﻿' <Snippet4>
-Module Example
-    Public Sub Main() 
-        Dim posValue As Double = 3.45
-        Dim negValue As Double = -3.45
+﻿'<snippet1>
+' This example demonstrates the Math.Round() method in conjunction 
+' with the MidpointRounding enumeration.
+Class Sample
+    Public Shared Sub Main() 
+        Dim result As Decimal = 0D
+        Dim posValue As Decimal = 3.45D
+        Dim negValue As Decimal = -3.45D
         
-        ' Round a positive and a negative value using the default.  
-        Dim result As Double = Math.Round(posValue, 1)
-        Console.WriteLine("{0,4} = Math.Round({1,5}, 1)", result, posValue)
-        result = Math.Round(negValue, 1)
-        Console.WriteLine("{0,4} = Math.Round({1,5}, 1)", result, negValue)
-        Console.WriteLine()
-        
-        ' Round a positive value using a MidpointRounding value. 
+        ' Round a positive value using different strategies.
+        ' The precision of the result is 1 decimal place.
         result = Math.Round(posValue, 1, MidpointRounding.ToEven)
-        Console.WriteLine("{0,4} = Math.Round({1,5}, 1, MidpointRounding.ToEven)", 
+        Console.WriteLine("{0,4} = Math.Round({1,5}, 1, MidpointRounding.ToEven)", _
                            result, posValue)
         result = Math.Round(posValue, 1, MidpointRounding.AwayFromZero)
-        Console.WriteLine("{0,4} = Math.Round({1,5}, 1, MidpointRounding.AwayFromZero)", 
+        Console.WriteLine("{0,4} = Math.Round({1,5}, 1, MidpointRounding.AwayFromZero)", _
+                           result, posValue)
+        result = Math.Round(posValue, 1, MidpointRounding.ToZero)
+        Console.WriteLine("{0,4} = Math.Round({1,5}, 1, MidpointRounding.ToZero)", _
                            result, posValue)
         Console.WriteLine()
         
-        ' Round a positive value using a MidpointRounding value. 
+        ' Round a negative value using different strategies.
+        ' The precision of the result is 1 decimal place.
         result = Math.Round(negValue, 1, MidpointRounding.ToEven)
-        Console.WriteLine("{0,4} = Math.Round({1,5}, 1, MidpointRounding.ToEven)", 
+        Console.WriteLine("{0,4} = Math.Round({1,5}, 1, MidpointRounding.ToEven)", _
                             result, negValue)
         result = Math.Round(negValue, 1, MidpointRounding.AwayFromZero)
-        Console.WriteLine("{0,4} = Math.Round({1,5}, 1, MidpointRounding.AwayFromZero)", 
+        Console.WriteLine("{0,4} = Math.Round({1,5}, 1, MidpointRounding.AwayFromZero)", _
+                           result, negValue)
+        result = Math.Round(negValue, 1, MidpointRounding.ToZero)
+        Console.WriteLine("{0,4} = Math.Round({1,5}, 1, MidpointRounding.ToZero)", _
                            result, negValue)
         Console.WriteLine()
+    
     End Sub
-End Module
-' The example displays the following output:
-'       3.4 = Math.Round( 3.45, 1)
-'       -3.4 = Math.Round(-3.45, 1)
-'       
-'       3.4 = Math.Round( 3.45, 1, MidpointRounding.ToEven)
-'       3.5 = Math.Round( 3.45, 1, MidpointRounding.AwayFromZero)
-'       
-'       -3.4 = Math.Round(-3.45, 1, MidpointRounding.ToEven)
-'       -3.5 = Math.Round(-3.45, 1, MidpointRounding.AwayFromZero)
-' </Snippet4>
+End Class
+
+'This code example produces the following results:
+'
+'        3.4 = Math.Round(3.45, 1, MidpointRounding.ToEven)
+'        3.5 = Math.Round(3.45, 1, MidpointRounding.AwayFromZero)
+'        3.4 = Math.Round(3.45, 1, MidpointRounding.ToZero)
+'
+'        -3.4 = Math.Round(-3.45, 1, MidpointRounding.ToEven)
+'        -3.5 = Math.Round(-3.45, 1, MidpointRounding.AwayFromZero)
+'        -3.4 = Math.Round(-3.45, 1, MidpointRounding.ToZero)
+'
+'</snippet1>
