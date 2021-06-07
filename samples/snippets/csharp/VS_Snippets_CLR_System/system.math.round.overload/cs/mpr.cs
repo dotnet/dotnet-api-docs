edeab815a76d9c784f6fdb413670e44ae8fde015
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2021-06-07 10:40:28 -0700

    Improve midpoint-rounding related docs (#6785)

--- samples/snippets/csharp/VS_Snippets_CLR_System/system.math.round.overload/cs/mpr.cs
+++ samples/snippets/csharp/VS_Snippets_CLR_System/system.math.round.overload/cs/mpr.cs
@@ -1,43 +1,88 @@
-﻿using System;
+﻿// This example demonstrates the Math.Round() method in conjunction
+// with the MidpointRounding enumeration and decimal places.
+using System;
 
-class Sample
+class MPR
 {
-    public static void Main()
+    public static void DecimalExample()
     {
-      // <snippet4>
-       double posValue =  3.45;
-       double negValue = -3.45;
+        //<snippet1>
+        decimal result;
 
-       // Round a positive and a negative value using the default.
-       double result = Math.Round(posValue, 1);
-       Console.WriteLine("{0,4} = Math.Round({1,5}, 1)", result, posValue);
-       result = Math.Round(negValue, 1);
-       Console.WriteLine("{0,4} = Math.Round({1,5}, 1)\n", result, negValue);
+        // Round a positive value using different strategies.
+        // The precision of the result is 1 decimal place.
 
-       // Round a positive value using a MidpointRounding value.
-       result = Math.Round(posValue, 1, MidpointRounding.ToEven);
-       Console.WriteLine("{0,4} = Math.Round({1,5}, 1, MidpointRounding.ToEven)",
-                         result, posValue);
-       result = Math.Round(posValue, 1, MidpointRounding.AwayFromZero);
-       Console.WriteLine("{0,4} = Math.Round({1,5}, 1, MidpointRounding.AwayFromZero)\n",
-                         result, posValue);
+        result = Math.Round(3.45m, 1, MidpointRounding.ToEven);
+        Console.WriteLine($"{result} = Math.Round({3.45m}, 1, MidpointRounding.ToEven)");
+        result = Math.Round(3.45m, 1, MidpointRounding.AwayFromZero);
+        Console.WriteLine($"{result} = Math.Round({3.45m}, 1, MidpointRounding.AwayFromZero)");
+        result = Math.Round(3.47m, 1, MidpointRounding.ToZero);
+        Console.WriteLine($"{result} = Math.Round({3.47m}, 1, MidpointRounding.ToZero)\n");
 
-       // Round a negative value using a MidpointRounding value.
-       result = Math.Round(negValue, 1, MidpointRounding.ToEven);
-       Console.WriteLine("{0,4} = Math.Round({1,5}, 1, MidpointRounding.ToEven)",
-                         result, negValue);
-       result = Math.Round(negValue, 1, MidpointRounding.AwayFromZero);
-       Console.WriteLine("{0,4} = Math.Round({1,5}, 1, MidpointRounding.AwayFromZero)\n",
-                         result, negValue);
-      // The example displays the following output:
-      //        3.4 = Math.Round( 3.45, 1)
-      //       -3.4 = Math.Round(-3.45, 1)
-      //
-      //        3.4 = Math.Round( 3.45, 1, MidpointRounding.ToEven)
-      //        3.5 = Math.Round( 3.45, 1, MidpointRounding.AwayFromZero)
-      //
-      //       -3.4 = Math.Round(-3.45, 1, MidpointRounding.ToEven)
-      //       -3.5 = Math.Round(-3.45, 1, MidpointRounding.AwayFromZero)
-      // </Snippet4>
-   }
+        // Round a negative value using different strategies.
+        // The precision of the result is 1 decimal place.
+
+        result = Math.Round(-3.45m, 1, MidpointRounding.ToEven);
+        Console.WriteLine($"{result} = Math.Round({-3.45m}, 1, MidpointRounding.ToEven)");
+        result = Math.Round(-3.45m, 1, MidpointRounding.AwayFromZero);
+        Console.WriteLine($"{result} = Math.Round({-3.45m}, 1, MidpointRounding.AwayFromZero)");
+        result = Math.Round(-3.47m, 1, MidpointRounding.ToZero);
+        Console.WriteLine($"{result} = Math.Round({-3.47m}, 1, MidpointRounding.ToZero)\n");
+
+        /*
+        This code example produces the following results:
+
+        3.4 = Math.Round(3.45, 1, MidpointRounding.ToEven)
+        3.5 = Math.Round(3.45, 1, MidpointRounding.AwayFromZero)
+        3.4 = Math.Round(3.47, 1, MidpointRounding.ToZero)
+
+        -3.4 = Math.Round(-3.45, 1, MidpointRounding.ToEven)
+        -3.5 = Math.Round(-3.45, 1, MidpointRounding.AwayFromZero)
+        -3.4 = Math.Round(-3.47, 1, MidpointRounding.ToZero)
+        */
+
+        //</snippet1>
+    }
+
+    public static void DoubleExample()
+    {
+        // <snippet4>
+
+        // Round a positive and a negative value using the default.
+        double result = Math.Round(3.45, 1);
+        Console.WriteLine($"{result,4} = Math.Round({3.45,5}, 1)");
+        result = Math.Round(-3.45, 1);
+        Console.WriteLine($"{result,4} = Math.Round({-3.45,5}, 1)\n");
+
+        // Round a positive value using a MidpointRounding value.
+        result = Math.Round(3.45, 1, MidpointRounding.ToEven);
+        Console.WriteLine($"{result,4} = Math.Round({3.45,5}, 1, MidpointRounding.ToEven)");
+        result = Math.Round(3.45, 1, MidpointRounding.AwayFromZero);
+        Console.WriteLine($"{result,4} = Math.Round({3.45,5}, 1, MidpointRounding.AwayFromZero)");
+        result = Math.Round(3.47, 1, MidpointRounding.ToZero);
+        Console.WriteLine($"{result,4} = Math.Round({3.47,5}, 1, MidpointRounding.ToZero)\n");
+
+        // Round a negative value using a MidpointRounding value.
+        result = Math.Round(-3.45, 1, MidpointRounding.ToEven);
+        Console.WriteLine($"{result,4} = Math.Round({-3.45,5}, 1, MidpointRounding.ToEven)");
+        result = Math.Round(-3.45, 1, MidpointRounding.AwayFromZero);
+        Console.WriteLine($"{result,4} = Math.Round({-3.45,5}, 1, MidpointRounding.AwayFromZero)");
+        result = Math.Round(-3.47, 1, MidpointRounding.ToZero);
+        Console.WriteLine($"{result,4} = Math.Round({-3.47,5}, 1, MidpointRounding.ToZero)\n");
+
+        // The example displays the following output:
+
+        //         3.4 = Math.Round( 3.45, 1)
+        //         -3.4 = Math.Round(-3.45, 1)
+
+        //         3.4 = Math.Round(3.45, 1, MidpointRounding.ToEven)
+        //         3.5 = Math.Round(3.45, 1, MidpointRounding.AwayFromZero)
+        //         3.4 = Math.Round(3.47, 1, MidpointRounding.ToZero)
+
+        //         -3.4 = Math.Round(-3.45, 1, MidpointRounding.ToEven)
+        //         -3.5 = Math.Round(-3.45, 1, MidpointRounding.AwayFromZero)
+        //         -3.4 = Math.Round(-3.47, 1, MidpointRounding.ToZero)
+
+        // </snippet4>
+    }
 }
