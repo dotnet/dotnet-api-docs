b378cd576eb8bbdab4c8c392fa83f85d466fb30e
Thiago Viana Avelar De Souza <thide2001@gmail.com>
2021-11-08 12:27:22 -0300

    Fixed identation (#7365)

--- samples/snippets/csharp/VS_Snippets_CLR_System/system.threading.cancellationtokensource.class/cs/cancel1.cs
+++ samples/snippets/csharp/VS_Snippets_CLR_System/system.threading.cancellationtokensource.class/cs/cancel1.cs
@@ -20,43 +20,43 @@ public class Example
       for (int taskCtr = 0; taskCtr <= 10; taskCtr++) {
          int iteration = taskCtr + 1;
          tasks.Add(factory.StartNew( () => {
-                                       int value;
-                                       int[] values = new int[10];
-                                       for (int ctr = 1; ctr <= 10; ctr++) {
-                                          lock (lockObj) {
-                                             value = rnd.Next(0,101);
-                                          }
-                                          if (value == 0) { 
-                                             source.Cancel();
-                                             Console.WriteLine("Cancelling at task {0}", iteration);
-                                             break;
-                                          }   
-                                          values[ctr-1] = value; 
-                                       }
-                                       return values;
-                                    }, token));   
+           int value;
+           int[] values = new int[10];
+           for (int ctr = 1; ctr <= 10; ctr++) {
+              lock (lockObj) {
+                 value = rnd.Next(0,101);
+              }
+              if (value == 0) { 
+                 source.Cancel();
+                 Console.WriteLine("Cancelling at task {0}", iteration);
+                 break;
+              }   
+              values[ctr-1] = value; 
+           }
+           return values;
+        }, token));   
       }
       try {
          Task<double> fTask = factory.ContinueWhenAll(tasks.ToArray(), 
-                                                      (results) => {
-                                                         Console.WriteLine("Calculating overall mean...");
-                                                         long sum = 0;
-                                                         int n = 0; 
-                                                         foreach (var t in results) {
-                                                            foreach (var r in t.Result) {
-                                                                  sum += r;
-                                                                  n++;
-                                                               }
-                                                         }
-                                                         return sum/(double) n;
-                                                      } , token);
+         (results) => {
+            Console.WriteLine("Calculating overall mean...");
+            long sum = 0;
+            int n = 0; 
+            foreach (var t in results) {
+               foreach (var r in t.Result) {
+                  sum += r;
+                  n++;
+               }
+            }
+            return sum/(double) n;
+         } , token);
          Console.WriteLine("The mean is {0}.", fTask.Result);
       }   
       catch (AggregateException ae) {
          foreach (Exception e in ae.InnerExceptions) {
             if (e is TaskCanceledException)
                Console.WriteLine("Unable to compute mean: {0}", 
-                                 ((TaskCanceledException) e).Message);
+                  ((TaskCanceledException) e).Message);
             else
                Console.WriteLine("Exception: " + e.GetType().Name);
          }
