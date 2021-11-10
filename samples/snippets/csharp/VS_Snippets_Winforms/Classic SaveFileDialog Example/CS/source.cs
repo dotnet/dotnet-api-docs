99683421681eb002df7132fc2df0eb8d68e19f16
Scott Michaud <scott@scott-michaud.com>
2021-11-09 21:49:40 -0500

    Dispose of SaveFileDialog when complete.
    SaveFileDialog implements IDisposable, and so it should be disposed when not used.
    
    Question: Should this be written in a C# 10 / .NET 6 `using SaveFileDialog saveFileDialog1 = new();` syntax?
    Question: Should OpenFile() also be migrated to using()?
    Question: Should we go the other direction and try/finally?

--- samples/snippets/csharp/VS_Snippets_Winforms/Classic SaveFileDialog Example/CS/source.cs
+++ samples/snippets/csharp/VS_Snippets_Winforms/Classic SaveFileDialog Example/CS/source.cs
@@ -10,19 +10,20 @@ public class Form1: Form
 // <Snippet1>
 private void button1_Click(object sender, System.EventArgs e)
  {
-     Stream myStream ;
-     SaveFileDialog saveFileDialog1 = new SaveFileDialog();
- 
-     saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"  ;
-     saveFileDialog1.FilterIndex = 2 ;
-     saveFileDialog1.RestoreDirectory = true ;
- 
-     if(saveFileDialog1.ShowDialog() == DialogResult.OK)
-     {
-         if((myStream = saveFileDialog1.OpenFile()) != null)
+     Stream myStream;
+     using (SaveFileDialog saveFileDialog1 = new SaveFileDialog())
+     { 
+         saveFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*"  ;
+         saveFileDialog1.FilterIndex = 2 ;
+         saveFileDialog1.RestoreDirectory = true ;
+
+         if(saveFileDialog1.ShowDialog() == DialogResult.OK)
          {
-             // Code to write the stream goes here.
-             myStream.Close();
+             if((myStream = saveFileDialog1.OpenFile()) != null)
+             {
+                 // Code to write the stream goes here.
+                 myStream.Close();
+             }
          }
      }
  }
