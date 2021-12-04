72f8b0215e13dcd7e844eaf18ece1309c47f1bf1
albert-du <52804499+albert-du@users.noreply.github.com>
2021-12-03 20:38:30 -0800

    Add F# snippets

---
+++ samples/snippets/fsharp/VS_Snippets_CLR_System/system.Int32.Parse/fs/Parse1.fs
@@ -0,0 +1,39 @@
+﻿module Parse1
+
+// <Snippet1>
+open System
+
+let values =
+    [ "+13230"; "-0"; "1,390,146"; "$190,235,421,127"
+      "0xFA1B"; "163042"; "-10"; "007"; "2147483647"
+      "2147483648"; "16e07"; "134985.0"; "-12034"
+      "-2147483648"; "-2147483649" ]
+
+for value in values do
+    try
+        let number = Int32.Parse value
+        printfn $"{value} --> {number}"
+    with 
+    | :? FormatException ->
+        printfn $"{value}: Bad Format"
+    | :? OverflowException ->
+        printfn $"{value}: Overflow"
+    
+
+// The example displays the following output:
+//       +13230 --> 13230
+//       -0 --> 0
+//       1,390,146: Bad Format
+//       $190,235,421,127: Bad Format
+//       0xFA1B: Bad Format
+//       163042 --> 163042
+//       -10 --> -10
+//       007 --> 7
+//       2147483647 --> 2147483647
+//       2147483648: Overflow
+//       16e07: Bad Format
+//       134985.0: Bad Format
+//       -12034 --> -12034
+//       -2147483648 --> -2147483648
+//       -2147483649: Overflow
+// </Snippet1>
