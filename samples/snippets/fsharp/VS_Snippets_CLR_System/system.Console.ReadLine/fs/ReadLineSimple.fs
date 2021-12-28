f6af3fc5534faa1ec91b13249673730bdf5cc5e3
albert-du <52804499+albert-du@users.noreply.github.com>
2021-12-28 14:10:51 -0800

    System.Console F# snippets

---
+++ samples/snippets/fsharp/VS_Snippets_CLR_System/system.Console.ReadLine/fs/ReadLineSimple.fs
@@ -0,0 +1,19 @@
+﻿module ReadLineSimple
+
+// <Snippet6>
+open System
+
+Console.Clear()
+
+let dat = DateTime.Now
+
+printfn $"\nToday is {dat:d} at {dat:T}." 
+printf "\nPress any key to continue... "
+Console.ReadLine() |> ignore
+
+// The example displays output like the following:
+//     Today is 12/28/2021 at 8:23:50 PM.
+//
+//     Press any key to continue...
+
+// </Snippet6>
