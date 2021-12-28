09305bea551c0146fe6506b28b63435dcad3b810
albert-du <52804499+albert-du@users.noreply.github.com>
2021-12-28 11:21:54 -0800

    System.Char F# snippets

---
+++ samples/snippets/fsharp/VS_Snippets_CLR_System/system.char.toupper/fs/toupper1.fs
@@ -0,0 +1,18 @@
+﻿module toupper1
+
+// <Snippet1>
+open System
+
+let chars = [| 'e'; 'E'; '6'; ','; 'ж'; 'ä' |]
+
+for ch in chars do
+    printfn $"""{ch} --> {Char.ToUpper ch} {if ch = Char.ToUpper ch then "(Same Character)" else ""}"""
+
+// The example displays the following output:
+//       e --> E
+//       E --> E (Same Character)
+//       6 --> 6 (Same Character)
+//       , --> , (Same Character)
+//       ж --> Ж
+//       ä --> Ä
+// </Snippet1>
