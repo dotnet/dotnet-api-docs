1558662c7cd9d44e3e2f0adeb695aa3ce10df2bd
Albert Du <52804499+albert-du@users.noreply.github.com>
2022-01-06 06:23:46 -0800

    System.Char F# snippets (#7535)
    * System.Char F# snippets
    
    * fix build
    
    * Update utf.fs

---
+++ samples/snippets/fsharp/VS_Snippets_CLR_System/system.Char.IsNumber/FS/isnumber1.fs
@@ -0,0 +1,31 @@
+﻿module isnumber1
+
+open System
+
+let overload1 () =
+    // <Snippet1>
+    let utf32 = 0x10107      // AEGEAN NUMBER ONE
+    let surrogate = Char.ConvertFromUtf32 utf32
+    for ch in surrogate do
+        printfn $"U+{Convert.ToUInt16 ch:X4}: {Char.IsNumber ch}"
+
+    // The example displays the following output:
+    //       U+D800: False
+    //       U+DD07: False
+    // </Snippet1>
+
+let overload2 () =
+    // <Snippet2>
+    let utf32 = 0x10107      // AEGEAN NUMBER ONE
+    let surrogate = Char.ConvertFromUtf32 utf32
+    for i = 0 to surrogate.Length - 1 do
+        printfn $"U+{Convert.ToUInt16 surrogate[i]:X4} at position {i}: {Char.IsNumber(surrogate, i)}"
+                        
+    // The example displays the following output:
+    //       U+D800 at position 0: True
+    //       U+DD07 at position 1: False
+    //  </Snippet2>
+
+overload1 ()
+printfn ""
+overload2 ()
---
+++ samples/snippets/fsharp/VS_Snippets_CLR_System/system.Char.IsSeparator/FS/isseparator.fs
@@ -0,0 +1,11 @@
+﻿module isseparator
+
+// <snippet10>
+open System
+
+let str = "twain1 twain2"
+
+printfn $"{Char.IsSeparator 'a'}"       // Output: "False"
+printfn $"{Char.IsSeparator(str, 6)}"   // Output: "True"
+
+// </snippet10>
