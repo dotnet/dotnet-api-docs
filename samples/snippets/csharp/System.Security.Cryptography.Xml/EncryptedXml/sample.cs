6cde77cad92003230e0ac6548a7869dc4426002c
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2022-01-07 17:19:12 -0800

    fix projects

--- samples/snippets/csharp/System.Security.Cryptography.Xml/EncryptedXml/sample.cs
+++ samples/snippets/csharp/System.Security.Cryptography.Xml/EncryptedXml/sample.cs
@@ -3,29 +3,29 @@ using System.Xml;
 using System.Security.Cryptography;
 using System.Security.Cryptography.Xml;
 
-    //<SNIPPET1>
-    public static void Decrypt(XmlDocument Doc, RSA Alg, string KeyName)
+//<SNIPPET1>
+static void Decrypt(XmlDocument Doc, RSA Alg, string KeyName)
+{
+    // Check the arguments.
+    if (Doc == null)
+        throw new ArgumentNullException("Doc");
+    if (Alg == null)
+        throw new ArgumentNullException("Alg");
+    if (KeyName == null)
+        throw new ArgumentNullException("KeyName");
+
+    // Create a new EncryptedXml object.
+    var exml = new EncryptedXml(Doc);
+
+    // Add a key-name mapping.
+    // This method can only decrypt documents
+    // that present the specified key name.
+    exml.AddKeyNameMapping(KeyName, Alg);
+
+    while (Doc.GetElementsByTagName("EncryptedData", EncryptedXml.XmlEncNamespaceUrl).Count > 0)
     {
-        // Check the arguments.
-        if (Doc == null)
-            throw new ArgumentNullException("Doc");
-        if (Alg == null)
-            throw new ArgumentNullException("Alg");
-        if (KeyName == null)
-            throw new ArgumentNullException("KeyName");
-
-        // Create a new EncryptedXml object.
-        EncryptedXml exml = new EncryptedXml(Doc);
-
-        // Add a key-name mapping.
-        // This method can only decrypt documents
-        // that present the specified key name.
-        exml.AddKeyNameMapping(KeyName, Alg);
-
-        while (Doc.GetElementsByTagName("EncryptedData", EncryptedXml.XmlEncNamespaceUrl).Count > 0)
-        {
-            // Decrypt the element.
-            exml.DecryptDocument();
-        }
+        // Decrypt the element.
+        exml.DecryptDocument();
     }
-    //</SNIPPET1>
+}
+//</SNIPPET1>
