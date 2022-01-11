7fbf8c84e9a7955c3b180712e9d8308f83a7ebfa
Richard Green <richard.green@15below.com>
2022-01-11 00:04:33 +0000

    Add using to SHA256 example (#7481)

--- samples/snippets/csharp/VS_Snippets_CLR_Classic/classic SHA256 Example/CS/source.cs
+++ samples/snippets/csharp/VS_Snippets_CLR_Classic/classic SHA256 Example/CS/source.cs
@@ -1,11 +1,9 @@
-﻿//<Snippet1>
-using System;
-using System.IO;
+//<Snippet1>
 using System.Security.Cryptography;
 
 public class HashDirectory
 {
-    public static void Main(String[] args)
+    public static void Main(string[] args)
     {
         if (args.Length < 1)
         {
@@ -17,7 +15,7 @@ public class HashDirectory
         if (Directory.Exists(directory))
         {
             // Create a DirectoryInfo object representing the specified directory.
-           var dir = new DirectoryInfo(directory);
+            var dir = new DirectoryInfo(directory);
             // Get the FileInfo objects for every file in the directory.
             FileInfo[] files = dir.GetFiles();
             // Initialize a SHA256 hash object.
@@ -26,24 +24,27 @@ public class HashDirectory
                 // Compute and print the hash values for each file in directory.
                 foreach (FileInfo fInfo in files)
                 {
-                    try {
-                        // Create a fileStream for the file.
-                        FileStream fileStream = fInfo.Open(FileMode.Open);
-                        // Be sure it's positioned to the beginning of the stream.
-                        fileStream.Position = 0;
-                        // Compute the hash of the fileStream.
-                        byte[] hashValue = mySHA256.ComputeHash(fileStream);
-                        // Write the name and hash value of the file to the console.
-                        Console.Write($"{fInfo.Name}: ");
-                        PrintByteArray(hashValue);
-                        // Close the file.
-                        fileStream.Close();
-                    }
-                    catch (IOException e) {
-                        Console.WriteLine($"I/O Exception: {e.Message}");
-                    }
-                    catch (UnauthorizedAccessException e) {
-                        Console.WriteLine($"Access Exception: {e.Message}");
+                    using (FileStream fileStream = fInfo.Open(FileMode.Open))
+                    {
+                        try
+                        {
+                            // Create a fileStream for the file.
+                            // Be sure it's positioned to the beginning of the stream.
+                            fileStream.Position = 0;
+                            // Compute the hash of the fileStream.
+                            byte[] hashValue = mySHA256.ComputeHash(fileStream);
+                            // Write the name and hash value of the file to the console.
+                            Console.Write($"{fInfo.Name}: ");
+                            PrintByteArray(hashValue);
+                        }
+                        catch (IOException e)
+                        {
+                            Console.WriteLine($"I/O Exception: {e.Message}");
+                        }
+                        catch (UnauthorizedAccessException e)
+                        {
+                            Console.WriteLine($"Access Exception: {e.Message}");
+                        }
                     }
                 }
             }
