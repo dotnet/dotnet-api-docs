e54b94f98ed80afb9612de7191ddf18c7de1fa3e
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2022-01-11 13:11:36 -0800

    reorganize System.Diagnostics snippets and update refs

---
+++ samples/snippets/csharp/System.Diagnostics/Debug/WriteLineIf/source3.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+public class Form4 : Form
+{
+    protected TextBox textBox1;
+    // <Snippet1>
+    // Class-level declaration.
+    // Create a TraceSwitch.
+    static TraceSwitch generalSwitch = new TraceSwitch("General", "Entire Application");
+
+    static public void MyErrorMethod(Object myObject, string category)
+    {
+        // Write the message if the TraceSwitch level is set to Error or higher.
+        Debug.WriteIf(generalSwitch.TraceError, "Invalid object for category. ");
+
+        // Write a second message if the TraceSwitch level is set to Verbose.
+        Debug.WriteLineIf(generalSwitch.TraceVerbose, myObject, category);
+    }
+
+    // </Snippet1>
+}
---
+++ samples/snippets/csharp/System.Diagnostics/EventLog/Delete/source1.cs
@@ -0,0 +1,33 @@
+﻿// <Snippet1>
+using System;
+using System.Diagnostics;
+using System.Threading;
+
+class MySample1
+{
+    public static void Main()
+    {
+        string logName;
+
+        if (EventLog.SourceExists("MySource"))
+        {
+            // Find the log associated with this source.
+            logName = EventLog.LogNameFromSourceName("MySource", ".");
+            // Make sure the source is in the log we believe it to be in.
+            if (logName != "MyLog")
+                return;
+            // Delete the source and the log.
+            EventLog.DeleteEventSource("MySource");
+            EventLog.Delete(logName);
+
+            Console.WriteLine(logName + " deleted.");
+        }
+        else
+        {
+            // Create the event source to make next try successful.
+            EventLog.CreateEventSource("MySource", "MyLog");
+        }
+    }
+}
+
+// </Snippet1>
