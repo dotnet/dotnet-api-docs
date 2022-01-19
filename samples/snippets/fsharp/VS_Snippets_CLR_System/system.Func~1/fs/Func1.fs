781ebb12b9c20f7e13c3fbc7a5d8f98569750c1d
Albert Du <52804499+albert-du@users.noreply.github.com>
2022-01-19 06:00:56 -0800

    Func<TResult> F# snippets (#7607)

---
+++ samples/snippets/fsharp/VS_Snippets_CLR_System/system.Func~1/fs/Func1.fs
@@ -0,0 +1,24 @@
+﻿module Func1
+
+// <Snippet2>
+open System
+open System.IO
+
+type OutputTarget() =
+    member _.SendToFile() =
+        try
+            let fn = Path.GetTempFileName()
+            use sw = new StreamWriter(fn)
+            sw.WriteLine "Hello, World!"
+            true
+        with _ ->
+            false
+
+let output = OutputTarget()
+let methodCall = Func<bool> output.SendToFile
+if methodCall.Invoke() then
+    printfn "Success!"
+else
+    printfn "File write operation failed."
+
+// </Snippet2>
