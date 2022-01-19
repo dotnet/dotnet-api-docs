2fe31d2797ac2e83c52b365661d7ad3d6c1ef4ee
albert-du <52804499+albert-du@users.noreply.github.com>
2022-01-18 17:22:21 -0800

    Update ctors.fs

--- samples/snippets/fsharp/VS_Snippets_CLR_System/system.Decimal.Ctor.Reals/FS/ctors.fs
+++ samples/snippets/fsharp/VS_Snippets_CLR_System/system.Decimal.Ctor.Reals/FS/ctors.fs
@@ -24,44 +24,35 @@ let createDecimal (value: float32) valToStr =
         // Display the exception type if an exception was thrown.
         printfn $"{getExceptionType ex,31}"
 
-    public static void Main( )
-    {
 
-        Console.WriteLine( "This example of the decimal( float ) " +
-            "constructor \ngenerates the following output.\n" );
-        Console.WriteLine( "{0,-27}{1,31}", "Constructor",
-            "Value or Exception" );
-        Console.WriteLine( "{0,-27}{1,31}", "-----------",
-            "------------------" );
+printfn "This example of the decimal( float ) constructor \ngenerates the following output.\n"
+printfn $"""{"Constructor",-27}{"Value or Exception",31}"""
+printfn $"""{"-----------",-27}{"------------------",31}"""
 
-        // Construct decimal objects from float values.
-        CreateDecimal( 1.2345E+5F, "1.2345E+5F" );
-        CreateDecimal( 1.234567E+15F, "1.234567E+15F" );
-        CreateDecimal( 1.23456789E+25F, "1.23456789E+25F" );
-        CreateDecimal( 1.23456789E+35F, "1.23456789E+35F" );
-        CreateDecimal( 1.2345E-5F, "1.2345E-5F" );
-        CreateDecimal( 1.234567E-15F, "1.234567E-15F" );
-        CreateDecimal( 1.23456789E-25F, "1.23456789E-25F" );
-        CreateDecimal( 1.23456789E-35F, "1.23456789E-35F" );
-        CreateDecimal( 1.0F / 7.0F, "1.0F / 7.0F" );
-    }
-}
+// Construct decimal objects from float values.
+createDecimal 1.2345E+5f "1.2345E+5F"
+createDecimal 1.234567E+15f "1.234567E+15F"
+createDecimal 1.23456789E+25f "1.23456789E+25F"
+createDecimal 1.23456789E+35f "1.23456789E+35F"
+createDecimal 1.2345E-5f "1.2345E-5F"
+createDecimal 1.234567E-15f "1.234567E-15F"
+createDecimal 1.23456789E-25f "1.23456789E-25F"
+createDecimal 1.23456789E-35f "1.23456789E-35F"
+createDecimal (1f / 7f) "1.0F / 7.0F"
 
-/*
-This example of the decimal( float ) constructor
-generates the following output.
 
-Constructor                             Value or Exception
------------                             ------------------
-decimal( 1.2345E+5F )                               123450
-decimal( 1.234567E+15F )                  1234567000000000
-decimal( 1.23456789E+25F )      12345680000000000000000000
-decimal( 1.23456789E+35F )               OverflowException
-decimal( 1.2345E-5F )                          0.000012345
-decimal( 1.234567E-15F )           0.000000000000001234567
-decimal( 1.23456789E-25F )  0.0000000000000000000000001235
-decimal( 1.23456789E-35F )                               0
-decimal( 1.0F / 7.0F )                           0.1428571
-*/
+// This example of the decimal( float ) constructor
+// generates the following output.
+
+// Constructor                             Value or Exception
+// -----------                             ------------------
+// decimal( 1.2345E+5F )                               123450
+// decimal( 1.234567E+15F )                  1234567000000000
+// decimal( 1.23456789E+25F )      12345680000000000000000000
+// decimal( 1.23456789E+35F )               OverflowException
+// decimal( 1.2345E-5F )                          0.000012345
+// decimal( 1.234567E-15F )           0.000000000000001234567
+// decimal( 1.23456789E-25F )  0.0000000000000000000000001235
+// decimal( 1.23456789E-35F )                               0
+// decimal( 1.0F / 7.0F )                           0.1428571
 //</Snippet1>
-// Copied from /samples/snippets/csharp/VS_Snippets_CLR_System/system.Decimal.Ctor.Reals/CS/ctors.cs
