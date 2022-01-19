c3e822ab49b752fe37b2ac8eae90058fc4633ac1
albert-du <52804499+albert-du@users.noreply.github.com>
2022-01-19 08:12:06 -0800

    Func<T,TResult> F# snippets

---
+++ samples/snippets/fsharp/VS_Snippets_CLR_System/system.Func~2/fs/Example.fs
@@ -0,0 +1,26 @@
+﻿module Example
+
+// <Snippet5>
+open System
+open System.Linq
+
+// Declare a Func variable and assign a lambda expression to the
+// variable. The function takes a string and converts it to uppercase.
+let selector = Func<string, string>(fun str -> str.ToUpper())
+
+// Create a list of strings.
+let words = [ "orange"; "apple"; "Article"; "elephant" ]
+
+// Query the list and select strings according to the selector function.
+let aWords = words.Select selector
+
+// Output the results to the console.
+for word in aWords do
+    printfn $"{word}"
+
+// This code example produces the following output:
+//     ORANGE
+//     APPLE
+//     ARTICLE
+//     ELEPHANT
+// </Snippet5>
