106b3c570cebca760b46ed5518ca6d2a4c310fce
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2022-01-19 21:43:48 -0800

    reorganize snippets

---
+++ samples/snippets/csharp/System.IO/StreamWriter/AutoFlush/example23.cs
@@ -0,0 +1,23 @@
+﻿// <Snippet23>
+using System.IO;
+
+namespace ConsoleApplication
+{
+    class Program4
+    {
+        static void Main()
+        {
+            WriteCharacters();
+        }
+
+        static async void WriteCharacters()
+        {
+            using (StreamWriter writer = File.CreateText("newfile.txt"))
+            {
+                await writer.WriteLineAsync("First line of example");
+                await writer.WriteLineAsync("and second line");
+            }
+        }
+    }
+}
+// </Snippet23>
