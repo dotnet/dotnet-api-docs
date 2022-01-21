de0b1d85b31524fc7150353f17cdaddfcf9086b0
Albert Du <52804499+albert-du@users.noreply.github.com>
2022-01-21 12:19:15 -0800

    System.DateTime F# snippets (#7548)
    * System.DateTime F# snippets
    
    * Add missing module declaration

---
+++ samples/snippets/fsharp/VS_Snippets_CLR/DateTime.Add/FS/class1.fs
@@ -0,0 +1,9 @@
+﻿open System
+
+// <Snippet1>
+// Calculate what day of the week is 36 days from this instant.
+let today = DateTime.Now
+let duration = TimeSpan(36, 0, 0, 0)
+let answer = today.Add duration
+printfn $"{answer:dddd}"
+// </Snippet1>
