fcbb191d76d0f081cfda6306ba0d6cd236d34089
Albert Du <52804499+albert-du@users.noreply.github.com>
2022-01-24 08:02:06 -0800

    Func<T1,T2,T3,TResult> F# snippets (#7622)

---
+++ samples/snippets/fsharp/System/FuncT1,T2,T3,TResult/Overview/Func4.fs
@@ -0,0 +1,19 @@
+﻿module Func4
+
+// <Snippet2>
+open System
+open System.Globalization
+
+let parseInt (str: string) styles format = Int32.Parse(str, styles, format)
+
+let numericString = "-1,234"
+
+let parser =
+    Func<string, NumberStyles, IFormatProvider, int> parseInt
+
+parser.Invoke(
+    numericString,
+    NumberStyles.Integer ||| NumberStyles.AllowThousands,
+    CultureInfo.InvariantCulture )
+|> printfn "%i"
+// </Snippet2>
