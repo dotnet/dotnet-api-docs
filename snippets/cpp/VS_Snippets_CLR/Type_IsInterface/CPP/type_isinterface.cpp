13d97c7e536ce822c000883dba873a79d5d6e841
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2022-01-25 15:59:58 -0800

    Remove top-level "samples" folder (#7645)

---
+++ snippets/cpp/VS_Snippets_CLR/Type_IsInterface/CPP/type_isinterface.cpp
@@ -0,0 +1,35 @@
+
+// <Snippet1>
+using namespace System;
+
+// Declare an interface.
+interface class myIFace{};
+public ref class MyIsInterface{};
+
+void main()
+{
+   try
+   {
+      // Get the IsInterface attribute for myIFace.
+      bool myBool1 = myIFace::typeid->IsInterface;
+      
+      //Display the IsInterface attribute for myIFace.
+      Console::WriteLine( "Is the specified type an interface? {0}.", myBool1 );
+      
+      // Get the attribute IsInterface for MyIsInterface.
+      bool myBool2 = MyIsInterface::typeid->IsInterface;
+      
+      //Display the IsInterface attribute for MyIsInterface.
+      Console::WriteLine( "Is the specified type an interface? {0}.", myBool2 );
+   }
+   catch ( Exception^ e ) 
+   {
+      Console::WriteLine( "\nAn exception occurred: {0}.", e->Message );
+   }
+}
+/* The example produces the following output:
+
+Is the specified type an interface? True.
+Is the specified type an interface? False.
+*/
+// </Snippet1>
---
+++ snippets/visualbasic/VS_Snippets_CLR/Type_IsInterface/VB/type_isinterface.vb
@@ -0,0 +1,22 @@
+﻿' <Snippet1>
+' Declare an interface.
+Interface myIFace
+End Interface
+
+Class MyIsInterface
+    Public Shared Sub Main()
+        ' Get the IsInterface attribute for myIFace.
+        Dim myBool1 As Boolean = GetType(myIFace).IsInterface
+        Console.WriteLine("Is the specified type an interface? {0}.", myBool1)
+
+        ' Determine whether Example is an interface.
+        Dim myBool2 As Boolean = GetType(MyIsInterface).IsInterface
+        Console.WriteLine("Is the specified type an interface? {0}.", myBool2)
+        Console.ReadLine()
+
+    End Sub
+End Class
+' The example displays the following output:
+'       Is the specified type an interface? True.
+'       Is the specified type an interface? False.
+' </Snippet1>
