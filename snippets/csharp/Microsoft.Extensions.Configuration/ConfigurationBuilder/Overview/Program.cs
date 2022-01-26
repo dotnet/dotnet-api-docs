4005b5a17144daf4664db18e03e068689ad8a118
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2022-01-25 17:19:51 -0800

    add configurationbuilder example

---
+++ snippets/csharp/Microsoft.Extensions.Configuration/ConfigurationBuilder/Overview/Program.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder();
+IConfigurationBuilder builder = config.AddJsonFile("appsettings.json", false, true);
+IConfigurationRoot root = builder.Build();
+
+Console.WriteLine($"Hello, { root["weather"] } world!");
