b58b8d00731fe3d1c334aaed73b3d51f469df12f
albert-du <52804499+albert-du@users.noreply.github.com>
2022-02-06 12:14:12 -0800

    Enum F# snippets

---
+++ snippets/fsharp/System/Enum/Equals/enumequals1.fs
@@ -0,0 +1,25 @@
+﻿module enumequals1
+
+// <Snippet1>
+type SledDog =
+    | Unknown = 0
+    | AlaskanMalamute = 1
+    | Malamute = 1
+    | Husky = 2
+    | SiberianHusky = 2
+
+type WorkDog =
+    | Unknown = 0
+    | Newfoundland = 1
+    | GreatPyrennes = 2
+
+let dog1 = SledDog.Malamute
+let dog2 = SledDog.AlaskanMalamute
+let dog3 = WorkDog.Newfoundland
+
+printfn $"{dog1:F} ({dog1:D}) = {dog2:F} ({dog2:D}): {dog1.Equals dog2}"
+printfn $"{dog1:F} ({dog1:D}) = {dog3:F} ({dog3:D}): {dog1.Equals dog3}"
+// The example displays the following output:
+//       Malamute (1) = Malamute (1): True
+//       Malamute (1) = Newfoundland (1): False
+// </Snippet1>
---
+++ snippets/fsharp/System/Enum/GetValues/getvalues1.fs
@@ -0,0 +1,17 @@
+﻿module getvalues1
+
+// <Snippet1>
+open System
+
+type SignMagnitude =
+    | Negative = -1
+    | Zero = 0
+    | Positive = 1
+
+for value in Enum.GetValues typeof<SignMagnitude> do
+    printfn $"{value :?> int,3}     0x{value :?> int:X8}     {value :?> SignMagnitude}"
+// The example displays the following output:
+//         0     0x00000000     Zero
+//         1     0x00000001     Positive
+//        -1     0xFFFFFFFF     Negative
+// </Snippet1>
