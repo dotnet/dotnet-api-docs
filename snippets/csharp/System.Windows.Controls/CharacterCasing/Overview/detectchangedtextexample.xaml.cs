a9609bb7852fb815b1b9ebff6b5889bfcf9b21da
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2022-02-09 10:09:47 -0800

    Reorganize C# WPF snippets (#7681)

---
+++ snippets/csharp/System.Windows.Controls/CharacterCasing/Overview/detectchangedtextexample.xaml.cs
@@ -0,0 +1,24 @@
+﻿// <SnippetDetectChangedTextCodeExampleWholePage>
+using System.Windows.Controls;
+
+namespace SDKSample
+{
+    public partial class DetectChangedTextExample : Page
+    {
+        // This is a counter for the number of times the TextChanged fires
+        // for the tbCountingChanges TextBox.
+        private int uiChanges = 0;
+
+        // Event handler for TextChanged Event.
+        private void textChangedEventHandler(object sender, TextChangedEventArgs args)
+        {
+
+            uiChanges++;
+            if (tbCounterText != null)
+            {
+                tbCounterText.Text = uiChanges.ToString();
+            }
+        }
+    }
+}
+// </SnippetDetectChangedTextCodeExampleWholePage>
