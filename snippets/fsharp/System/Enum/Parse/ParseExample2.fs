35da196e066b71107976ef7d5bedb04a88543551
Albert Du <52804499+albert-du@users.noreply.github.com>
2022-02-15 12:24:37 -0800

    Enum F# snippets (#7696)

---
+++ snippets/fsharp/System/Enum/Parse/ParseExample2.fs
@@ -0,0 +1,32 @@
+﻿module PraseExample2
+
+// <Snippet2>
+open System
+
+[<Flags>]
+type Colors =
+    | None = 0
+    | Red = 1
+    | Green = 2
+    | Blue = 4
+
+let colorStrings = [ "0"; "2"; "8"; "blue"; "Blue"; "Yellow"; "Red, Green" ]
+for colorString in colorStrings do
+    try
+        let colorValue = Enum.Parse(typeof<Colors>, colorString, true) :?> Colors
+        if Enum.IsDefined(typeof<Colors>, colorValue) || (string colorValue).Contains "," then
+            printfn $"Converted '{colorString}' to {colorValue}."
+        else
+            printfn $"{colorString} is not an underlying value of the Colors enumeration."
+    with :? ArgumentException ->
+        printfn $"{colorString} is not a member of the Colors enumeration."
+
+// The example displays the following output:
+//       Converted '0' to None.
+//       Converted '2' to Green.
+//       8 is not an underlying value of the Colors enumeration.
+//       Converted 'blue' to Blue.
+//       Converted 'Blue' to Blue.
+//       Yellow is not a member of the Colors enumeration.
+//       Converted 'Red, Green' to Red, Green.
+// </Snippet2>
