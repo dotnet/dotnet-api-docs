ce780212fdf2866ed45cbfe115eb4b81d320e6f3
albert-du <52804499+albert-du@users.noreply.github.com>
2022-02-15 17:55:54 -0800

    FormatException F# snippets

---
+++ snippets/fsharp/System/FormatException/Overview/qa1.fs
@@ -0,0 +1,24 @@
+﻿module qa1
+
+// <Snippet5>
+open System
+
+let rnd = Random()
+let numbers = Array.zeroCreate<int> 4
+let mutable total = 0
+for i = 0 to 2 do
+    let number = rnd.Next 1001
+    numbers[i] <- number
+    total <- total + number
+numbers[3] <- total
+Console.WriteLine("{0} + {1} + {2} = {3}", numbers)
+
+// The example displays the following output:
+//    Unhandled Exception:
+//    System.FormatException:
+//       Index (zero based) must be greater than or equal to zero and less than the size of the argument list.
+//       at System.Text.StringBuilder.AppendFormat(IFormatProvider provider, String format, Object[] args)
+//       at System.IO.TextWriter.WriteLine(String format, Object arg0)
+//       at System.IO.TextWriter.SyncTextWriter.WriteLine(String format, Object arg0)
+//       at <StartupCode$fs>.$Example.main@()
+// </Snippet5>
