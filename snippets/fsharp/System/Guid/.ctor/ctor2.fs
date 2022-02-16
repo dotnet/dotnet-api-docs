a4f6a6ee05e7a0cecb0b6bc8f074efdd94e1c7f8
albert-du <52804499+albert-du@users.noreply.github.com>
2022-02-15 19:53:51 -0800

    Guid F# snippets

---
+++ snippets/fsharp/System/Guid/.ctor/ctor2.fs
@@ -0,0 +1,11 @@
+﻿module ctor2
+
+// <Snippet2>
+open System
+
+let g = Guid(0xA, 0xBs, 0xCs, [| 0uy..7uy |])
+printfn $"{g:B}"
+
+// The example displays the following output:
+//        {0000000a-000b-000c-0001-020304050607}
+// </Snippet2>
---
+++ snippets/fsharp/System/Guid/NewGuid/ng.fs
@@ -0,0 +1,12 @@
+﻿//<snippet1>
+open System
+
+// Create and display the value of two GUIDs.
+let g = Guid.NewGuid()
+printfn $"{g}"
+printfn $"{Guid.NewGuid()}"
+
+// This code example produces a result similar to the following:
+//     0f8fad5b-d9cb-469f-a165-70867728950e
+//     7c9e6679-7425-40de-944b-e07fc1f90ae7
+//</snippet1>
