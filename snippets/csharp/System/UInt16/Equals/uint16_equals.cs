4f977133933d0941a55321f0e9b0ab3154ea7016
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2022-02-17 16:12:31 -0800

    Add project files to System namespace (#7734)

--- snippets/csharp/System/UInt16/Equals/uint16_equals.cs
+++ snippets/csharp/System/UInt16/Equals/uint16_equals.cs
@@ -7,35 +7,36 @@
 */
 
 using System;
+
 class MyUInt16_Equals
 {
-     public static void Main()
-     {
-         try
-         {
-// <Snippet1>
+    public static void MyMethod()
+    {
+        try
+        {
+            // <Snippet1>
             UInt16 myVariable1 = 10;
             UInt16 myVariable2 = 10;
 
             //Display the declaring type.
-            Console.WriteLine("\nType of 'myVariable1' is '{0}' and"+
-                 " value is :{1}",myVariable1.GetType(), myVariable1);
-            Console.WriteLine("Type of 'myVariable2' is '{0}' and"+
-                 " value is :{1}",myVariable2.GetType(), myVariable2);
+            Console.WriteLine("\nType of 'myVariable1' is '{0}' and" +
+                 " value is :{1}", myVariable1.GetType(), myVariable1);
+            Console.WriteLine("Type of 'myVariable2' is '{0}' and" +
+                 " value is :{1}", myVariable2.GetType(), myVariable2);
 
             // Compare 'myVariable1' instance with 'myVariable2' Object.
-            if( myVariable1.Equals( myVariable2 ) )
-               Console.WriteLine( "\nStructures 'myVariable1' and "+
-                     "'myVariable2' are equal");
+            if (myVariable1.Equals(myVariable2))
+                Console.WriteLine("\nStructures 'myVariable1' and " +
+                      "'myVariable2' are equal");
             else
-               Console.WriteLine( "\nStructures 'myVariable1' and "+
-                     "'myVariable2' are not equal");
+                Console.WriteLine("\nStructures 'myVariable1' and " +
+                      "'myVariable2' are not equal");
 
-// </Snippet1>
-         }
-         catch(Exception e)
-         {
+            // </Snippet1>
+        }
+        catch (Exception e)
+        {
             Console.WriteLine("Exception :{0}", e.Message);
-         }
-     }
+        }
+    }
 }
