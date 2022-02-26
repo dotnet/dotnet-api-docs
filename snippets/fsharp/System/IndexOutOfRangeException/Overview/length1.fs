4d5acb685739b401451e52f4c060e39f29f7b4ec
albert-du <52804499+albert-du@users.noreply.github.com>
2022-02-25 23:03:11 -0800

    IndexOutOfRangeException F# snippets

---
+++ snippets/fsharp/System/IndexOutOfRangeException/Overview/length1.fs
@@ -0,0 +1,16 @@
+﻿module length1
+
+// <Snippet3>
+let characters = ResizeArray()
+characters.InsertRange(0, [| 'a'; 'b'; 'c'; 'd'; 'e'; 'f' |])
+
+for i = 0 to characters.Count do
+    printf $"'{characters[i]}'    "
+// The example displays the following output:
+//    'a'    'b'    'c'    'd'    'e'    'f'
+//    Unhandled Exception:
+//    System.ArgumentOutOfRangeException:
+//    Index was out of range. Must be non-negative and less than the size of the collection.
+//    Parameter name: index
+//       at <StartupCode$fs>.main@()
+// </Snippet3>
