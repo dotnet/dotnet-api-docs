1cb07a2a5c97fbb5dae4e48a138b0cdfd102b372
Albert Du <52804499+albert-du@users.noreply.github.com>
2022-02-28 08:09:59 -0800

    IndexOutOfRangeException F# snippets (#7771)

---
+++ snippets/fsharp/System/IndexOutOfRangeException/Overview/negative2.fs
@@ -0,0 +1,38 @@
+﻿module negative2
+
+// <Snippet6>
+open System
+open System.Collections.Generic
+
+let numbers = new List<int>()
+
+let showValues startValue =
+    // Create a collection with numeric values.
+    if numbers.Count = 0 then
+        numbers.AddRange [| 2..2..22 |]
+
+    // Get the index of startValue.
+    let startIndex = numbers.IndexOf startValue
+    if startIndex < 0 then
+        printfn $"Unable to find {startValue} in the collection."
+    else
+        // Display all numbers from startIndex on.
+        printfn $"Displaying values greater than or equal to {startValue}:"
+        for i = startIndex to numbers.Count - 1 do
+            printf $"    {numbers[i]}"
+
+let startValue =
+    let args = Environment.GetCommandLineArgs()
+    if args.Length < 2 then
+        2
+    else
+        match Int32.TryParse args[1] with
+        | true, v -> v
+        | _ -> 2
+
+showValues startValue
+
+// The example displays the following output if the user supplies
+// 7 as a command-line parameter:
+//      Unable to find 7 in the collection.
+// </Snippet6>
