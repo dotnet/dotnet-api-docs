64a185c5119cb12015248c8a5efee7ff737e1df9
Albert Du <52804499+albert-du@users.noreply.github.com>
2022-03-14 12:10:52 -0700

    OperatingSystem F# snippets (#7817)

---
+++ snippets/fsharp/System/OperatingSystem/ServicePack/sp.fs
@@ -0,0 +1,10 @@
+﻿//<snippet1>
+// This example demonstrates the OperatingSystem.ServicePack property.
+open System
+
+let os = Environment.OSVersion
+let sp = os.ServicePack
+printfn $"Service pack version = \"{sp}\""
+// This example produces the following results:
+//     Service pack version = "Service Pack 1"
+//</snippet1>
