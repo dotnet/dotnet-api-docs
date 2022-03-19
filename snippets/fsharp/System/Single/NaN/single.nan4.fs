7b3f2d8ed4a85342761a41ba85ce99cb2209f72b
albert-du <52804499+albert-du@users.noreply.github.com>
2022-03-19 13:02:28 -0700

    Single F# snippets

---
+++ snippets/fsharp/System/Single/NaN/single.nan4.fs
@@ -0,0 +1,37 @@
+﻿module nan4
+
+// <Snippet4>
+open System
+
+printfn $"NaN == NaN: {Single.NaN = Single.NaN}"
+printfn $"NaN != NaN: {Single.NaN <> Single.NaN}"
+printfn $"NaN.Equals(NaN): {Single.NaN.Equals Single.NaN}"
+printfn $"! NaN.Equals(NaN): {not (Single.NaN.Equals Single.NaN)}"
+printfn $"IsNaN: {Double.IsNaN Double.NaN}"
+
+printfn $"\nNaN > NaN: {Single.NaN > Single.NaN}"
+printfn $"NaN >= NaN: {Single.NaN >= Single.NaN}"
+printfn $"NaN < NaN: {Single.NaN < Single.NaN}"
+printfn $"NaN < 100.0: {Single.NaN < 100f}"
+printfn $"NaN <= 100.0: {Single.NaN <= 100f}"
+printfn $"NaN >= 100.0: {Single.NaN > 100f}"
+printfn $"NaN.CompareTo(NaN): {Single.NaN.CompareTo Single.NaN}"
+printfn $"NaN.CompareTo(100.0): {Single.NaN.CompareTo 100f}"
+printfn $"(100.0).CompareTo(Single.NaN): {100f.CompareTo Single.NaN}"
+// The example displays the following output:
+//       NaN == NaN: False
+//       NaN != NaN: True
+//       NaN.Equals(NaN): True
+//       ! NaN.Equals(NaN): False
+//       IsNaN: True
+//
+//       NaN > NaN: False
+//       NaN >= NaN: False
+//       NaN < NaN: False
+//       NaN < 100.0: False
+//       NaN <= 100.0: False
+//       NaN >= 100.0: False
+//       NaN.CompareTo(NaN): 0
+//       NaN.CompareTo(100.0): -1
+//       (100.0).CompareTo(Single.NaN): 1
+// </Snippet4>
