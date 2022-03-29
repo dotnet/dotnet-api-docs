f9dbb712a7876e6417b36c80397d62214111b046
albert-du <52804499+albert-du@users.noreply.github.com>
2022-03-28 20:04:48 -0700

    TimeSpan F# snippets

---
+++ snippets/fsharp/System/TimeSpan/TotalHours/totalhours.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+// <Snippet1>
+// Define an interval of 1 day, 15+ hours.
+let interval = TimeSpan(1, 15, 42, 45, 750) 
+printfn $"Value of TimeSpan: {interval}"
+
+printfn $"{interval.TotalHours:N5} hours, as follows:"
+printfn $"   Hours:        {interval.Days * 24 + interval.Hours,3}"
+printfn $"   Minutes:      {interval.Minutes,3}" 
+printfn $"   Seconds:      {interval.Seconds,3}" 
+printfn $"   Milliseconds: {interval.Milliseconds,3}"
+
+// The example displays the following output:
+//       Value of TimeSpan: 1.15:42:45.7500000
+//       39.71271 hours, as follows:
+//          Hours:         39
+//          Minutes:       42
+//          Seconds:       45
+//          Milliseconds: 750
+// </Snippet1>
