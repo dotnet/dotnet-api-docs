e977beec3fd4ce08825afc0a318b86154a6bf991
albert-du <52804499+albert-du@users.noreply.github.com>
2022-03-30 19:02:56 -0700

    TimeZoneInfo F# snippets

---
+++ snippets/fsharp/System/TimeZoneInfo/Equals/equals1.fs
@@ -0,0 +1,12 @@
+﻿// <Snippet1>
+open System
+
+let thisTimeZone = TimeZoneInfo.Local
+let obj1 = TimeZoneInfo.FindSystemTimeZoneById "Pacific Standard Time"
+let obj2 = TimeZoneInfo.FindSystemTimeZoneById "Eastern Standard Time"
+printfn $"{thisTimeZone.Equals obj1}"
+printfn $"{thisTimeZone.Equals obj2}"
+// The example displays the following output:
+//      True
+//      False
+// </Snippet1>
