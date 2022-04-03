3746dc016fe93278491bd4cbf3acb17ad5a59158
albert-du <52804499+albert-du@users.noreply.github.com>
2022-04-03 10:38:42 -0700

    AppDomain F# snippets

---
+++ snippets/fsharp/System/AppDomain/CreateInstanceAndUnwrap/ignorecase.fs
@@ -0,0 +1,27 @@
+﻿module ignorecase
+
+// <Snippet1>
+open System
+open System.Reflection
+
+
+let instantiateINT32 ignoreCase =
+    try
+        let currentDomain = AppDomain.CurrentDomain
+        let instance = currentDomain.CreateInstanceAndUnwrap(
+            "mscorlib, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089",
+            "SYSTEM.INT32",
+            ignoreCase,
+            BindingFlags.Default,
+            null,
+            null,
+            null,
+            null,
+            null)
+        printfn $"{instance.GetType()}"
+    with :? TypeLoadException as e ->
+        printfn $"{e.Message}"
+
+instantiateINT32 false     // Failed!
+instantiateINT32 true      // OK!
+// </Snippet1>
