4927a7a6a890a9b7b7dfe8de58f53945aa7a0e9b
albert-du <52804499+albert-du@users.noreply.github.com>
2022-04-08 17:27:59 -0700

    Tuple<T1, T2> F# snippets

---
+++ snippets/fsharp/System/TupleT1,T2/System.Collections.IStructuralComparable.CompareTo/compareto2.fs
@@ -0,0 +1,58 @@
+﻿module compareto2
+
+// <Snippet2>
+open System
+open System.Collections
+open System.Collections.Generic
+
+type ScoreComparer<'T1, 'T2>() =
+    interface IComparer with
+        member _.Compare(x: obj, y: obj) =
+            match x with
+            | :? Tuple<'T1, 'T2> as tX ->
+                let tY = y :?> Tuple<'T1, 'T2>
+                Comparer<'T2>.Default.Compare(tX.Item2, tY.Item2)             
+            | _ -> 0
+
+let scores = 
+    [| Tuple<string, Nullable<int>>("Jack", 78)
+       Tuple<string, Nullable<int>>("Abbey", 92) 
+       Tuple<string, Nullable<int>>("Dave", 88)
+       Tuple<string, Nullable<int>>("Sam", 91) 
+       Tuple<string, Nullable<int>>("Ed", Nullable())
+       Tuple<string, Nullable<int>>("Penelope", 82)
+       Tuple<string, Nullable<int>>("Linda", 99)
+       Tuple<string, Nullable<int>>("Judith", 84) |]
+
+printfn "The values in unsorted order:"
+for score in scores do
+    printfn $"{score}"
+
+printfn ""
+
+Array.Sort(scores, ScoreComparer<string, Nullable<int>>())
+
+printfn "The values in sorted order:"
+for score in scores do
+    printfn $"{score}"
+// The example displays the following output
+//       The values in unsorted order:
+//       (Jack, 78)
+//       (Abbey, 92)
+//       (Dave, 88)
+//       (Sam, 91)
+//       (Ed, )
+//       (Penelope, 82)
+//       (Linda, 99)
+//       (Judith, 84)
+//       
+//       The values in sorted order:
+//       (Ed, )
+//       (Jack, 78)
+//       (Penelope, 82)
+//       (Judith, 84)
+//       (Dave, 88)
+//       (Sam, 91)
+//       (Abbey, 92)
+//       (Linda, 99)
+// </Snippet2>
---
+++ snippets/fsharp/System/TupleT1,T2/ToString/tostring1.fs
@@ -0,0 +1,24 @@
+﻿//<Snippet1>
+open System
+
+let scores = 
+    [| Tuple<string, Nullable<int>>("Abbey", 92) 
+       Tuple<string, Nullable<int>>("Dave", 88)
+       Tuple<string, Nullable<int>>("Ed", Nullable())
+       Tuple<string, Nullable<int>>("Jack", 78)
+       Tuple<string, Nullable<int>>("Linda", 99)
+       Tuple<string, Nullable<int>>("Judith", 84)
+       Tuple<string, Nullable<int>>("Penelope", 82)
+       Tuple<string, Nullable<int>>("Sam", 91) |]
+for score in scores do
+    printfn $"{score.ToString()}"
+// The example displays the following output:
+//       (Abbey, 92)
+//       (Dave, 88)
+//       (Ed, )
+//       (Jack, 78)
+//       (Linda, 99)
+//       (Judith, 84)
+//       (Penelope, 82)
+//       (Sam, 91)
+// </Snippet1>
