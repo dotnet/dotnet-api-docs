93804c0e4f54ceebe563361aadb4b8f72c1a6ff2
Albert Du <52804499+albert-du@users.noreply.github.com>
2022-05-09 07:09:20 -0700

    UriBuilder F# snippets (#8057)

---
+++ snippets/fsharp/System/UriBuilder/Query/main.fs
@@ -0,0 +1,14 @@
+﻿// <snippet1>
+open System
+
+let baseUri = UriBuilder "http://www.contoso.com/default.aspx?Param1=7890"
+let queryToAppend = "param2=1234"
+
+baseUri.Query <-
+    if baseUri.Query <> null && baseUri.Query.Length > 1 then
+        // Note: In .NET Core and .NET 5+, you can simplify by removing
+        // the call to Substring(), which removes the leading "?" character.
+        baseUri.Query.Substring 1 + "&" + queryToAppend 
+    else
+        queryToAppend 
+    // </snippet1>
