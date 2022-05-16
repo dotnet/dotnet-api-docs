9ae5c8e40dc16b66258e444c76b0b8261ab79c3e
syrotkin <syrotkin@gmail.com>
2022-05-16 18:51:54 +0200

    move snippet to separate files for csharp and vb

---
+++ snippets/csharp/System.Threading/SemaphoreSlim/Overview/waitRelease.cs
@@ -0,0 +1,9 @@
+﻿// <Snippet2>
+// Enter semaphore by calling one of the Wait or WaitAsync methods.
+SemaphoreSlim.Wait()
+//
+// Execute code protected by the semaphore.
+//
+SemaphoreSlim.Release()
+
+// </Snippet2>
