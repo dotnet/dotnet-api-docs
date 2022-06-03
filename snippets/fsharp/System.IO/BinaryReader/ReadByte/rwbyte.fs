d9a825b5722305a9e6abf376c079b882d67b3a53
Albert Du <52804499+albert-du@users.noreply.github.com>
2022-06-03 06:25:16 -0700

    BinaryReader F# snippets (#8123)

---
+++ snippets/fsharp/System.IO/BinaryReader/ReadByte/rwbyte.fs
@@ -0,0 +1,33 @@
+﻿// <Snippet1>
+open System
+open System.IO
+
+// Create random data to write to the stream.
+let writeArray = Array.zeroCreate<byte> 1000
+Random().NextBytes writeArray
+
+let binWriter = new BinaryWriter(new MemoryStream())
+let binReader = new BinaryReader(binWriter.BaseStream)
+
+try
+    // Write the data to the stream.
+    printfn "Writing the data."
+    for i = 0 to writeArray.Length - 1 do
+        binWriter.Write writeArray[i]
+
+    // Set the stream position to the beginning of the stream.
+    binReader.BaseStream.Position <- 0
+
+    let mutable failed = false
+    // Read and verify the data from the stream.
+    for i = 0 to writeArray.Length - 1 do
+        if binReader.ReadByte() <> writeArray[i] then
+            printfn "Error writing the data."
+            failed <- true
+    if not failed then
+        printfn "The data was written and verified."
+
+// Catch the EndOfStreamException and write an error message.
+with :? EndOfStreamException as e ->
+    printfn $"Error writing the data.\n{e.GetType().Name}"
+// </Snippet1>
---
+++ snippets/fsharp/System.IO/BinaryReader/ReadChar/rwchar.fs
@@ -0,0 +1,26 @@
+﻿// <Snippet1>
+open System.IO
+
+let invalidPathChars = Path.GetInvalidPathChars()
+let memStream = new MemoryStream()
+let binWriter = new BinaryWriter(memStream)
+
+// Write to memory.
+binWriter.Write "Invalid file path characters are: "
+for i = 0 to invalidPathChars.Length - 1 do
+    binWriter.Write invalidPathChars[i]
+
+// Create the reader using the same MemoryStream
+// as used with the writer.
+let binReader = new BinaryReader(memStream)
+
+// Set Position to the beginning of the stream.
+memStream.Position <- 0
+
+// Read the data from memory and write it to the console.
+printf $"{binReader.ReadString()}"
+let memoryData = Array.zeroCreate<char> (int (memStream.Length - memStream.Position))
+for i = 0 to memoryData.Length - 1 do
+    memoryData[i] <- binReader.ReadChar()
+printfn $"{memoryData}"
+// </Snippet1>
