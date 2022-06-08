eab72fb28305a5d3a5e0e66e6a340cc548cac0c0
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2022-06-08 11:15:51 -0700

    Add project file and try.net

--- snippets/csharp/System.Text.RegularExpressions/Regex/Match/match2.cs
+++ snippets/csharp/System.Text.RegularExpressions/Regex/Match/match2.cs
@@ -1,18 +1,22 @@
-﻿// <Snippet2>
-using System;
+﻿using System;
 using System.Text.RegularExpressions;
 
-public class Example
+namespace Examples
 {
-   public static void Main()
-   {
-      string pattern = @"\ba\w*\b";
-      string input = "An extraordinary day dawns with each new day.";
-      Match m = Regex.Match(input, pattern, RegexOptions.IgnoreCase);
-      if (m.Success)
-         Console.WriteLine("Found '{0}' at position {1}.", m.Value, m.Index);
-   }
+    public class Example2
+    {
+        public static void MatchIgnoreCase()
+        {
+            // <Snippet2>
+            string pattern = @"\ba\w*\b";
+            string input = "An extraordinary day dawns with each new day.";
+            Match m = Regex.Match(input, pattern, RegexOptions.IgnoreCase);
+            if (m.Success)
+                Console.WriteLine("Found '{0}' at position {1}.", m.Value, m.Index);
+
+            // The example displays the following output:
+            //        Found 'An' at position 0.
+            // </Snippet2>
+        }
+    }
 }
-// The example displays the following output:
-//        Found 'An' at position 0.
-// </Snippet2>
