c9138311405a818e13dfddcd060607a976edfa47
albert-du <52804499+albert-du@users.noreply.github.com>
2022-07-04 12:51:44 -0700

    DirectoryInfo F# snippets

---
+++ snippets/fsharp/System.IO/DirectoryInfo/GetDirectories/directoryinfogetdirectories.fs
@@ -0,0 +1,15 @@
+﻿module directoryinfogetdirectories
+
+// <snippet1>
+open System.IO
+
+// Make a reference to a directory.
+let di = DirectoryInfo "c:\\"
+
+// Get a reference to each directory in that directory.
+let diArr = di.GetDirectories()
+
+// Display the names of the directories.
+for dri in diArr do
+    printfn $"{dri.Name}"
+// </snippet1>
---
+++ snippets/fsharp/System.IO/DirectoryInfo/GetDirectories/dirinfo getdirs2.fs
@@ -0,0 +1,17 @@
+﻿module dirinfo_getdirs2
+
+// <Snippet1>
+open System.IO
+
+try
+    let di = DirectoryInfo @"c:\"
+
+    // Get only subdirectories that contain the letter "p."
+    let dirs = di.GetDirectories "*p*"
+    printfn $"The number of directories containing the letter p is {dirs.Length}."
+
+    for diNext in dirs do
+        printfn $"The number of files in {diNext} is {diNext.GetFiles().Length}"
+with e ->
+    printfn $"The process failed: {e}"
+// </Snippet1>
