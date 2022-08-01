ae6a59d0c4ad6414ef190c91e35cb10328544e57
Pebaz <pebazium@gmail.com>
2022-08-01 11:29:02 -0700

    Fix inconsistent whitespace around method calls

--- snippets/cpp/VS_Snippets_Remoting/NCLResponse1/CPP/httpwebrequest1.cpp
+++ snippets/cpp/VS_Snippets_Remoting/NCLResponse1/CPP/httpwebrequest1.cpp
@@ -13,31 +13,31 @@ using namespace System::IO;
 int main()
 {
    array<String^>^args = Environment::GetCommandLineArgs();
-   HttpWebRequest^ request = dynamic_cast<HttpWebRequest^>(WebRequest::Create( args[ 1 ] ));
-   
+   HttpWebRequest^ request = dynamic_cast<HttpWebRequest^>(WebRequest::Create(args[ 1 ]));
+
    // Set some reasonable limits on resources used by this request
    request->MaximumAutomaticRedirections = 4;
    request->MaximumResponseHeadersLength = 4;
-   
+
    // Set credentials to use for this request.
    request->Credentials = CredentialCache::DefaultCredentials;
    HttpWebResponse^ response = dynamic_cast<HttpWebResponse^>(request->GetResponse());
-   Console::WriteLine( "Content length is {0}", response->ContentLength );
-   Console::WriteLine( "Content type is {0}", response->ContentType );
-   
+   Console::WriteLine("Content length is {0}", response->ContentLength);
+   Console::WriteLine("Content type is {0}", response->ContentType);
+
    // Get the stream associated with the response.
    Stream^ receiveStream = response->GetResponseStream();
-   
-   // Pipes the stream to a higher level stream reader with the required encoding format. 
-   StreamReader^ readStream = gcnew StreamReader( receiveStream,Encoding::UTF8 );
-   Console::WriteLine( "Response stream received." );
-   Console::WriteLine( readStream->ReadToEnd() );
+
+   // Pipes the stream to a higher level stream reader with the required encoding format.
+   StreamReader^ readStream = gcnew StreamReader(receiveStream,Encoding::UTF8);
+   Console::WriteLine("Response stream received.");
+   Console::WriteLine(readStream->ReadToEnd());
    response->Close();
    readStream->Close();
 }
 
 /*
-The output from this example will vary depending on the value passed into Main 
+The output from this example will vary depending on the value passed into Main
 but will be similar to the following:
 
 Content length is 1542
