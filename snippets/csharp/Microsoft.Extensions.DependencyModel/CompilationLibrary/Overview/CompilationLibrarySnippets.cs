5732e5f575869314bc864ade3b5a6f5ecd9303f9
MSDN.WhiteKnight <35516665+MSDN-WhiteKnight@users.noreply.github.com>
2022-08-20 22:02:09 +0500

    Add initial content

---
+++ snippets/csharp/Microsoft.Extensions.DependencyModel/CompilationLibrary/Overview/CompilationLibrarySnippets.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.DependencyModel;
+
+namespace CompilationLibrarySnippets
+{
+    static class CompilationLibrarySnippets
+    {
+        public static void SnippetPrintLibraries()
+        {
+            //<SnippetPrintLibraries>
+            foreach (CompilationLibrary lib in DependencyContext.Default.CompileLibraries)
+            {
+                Console.WriteLine($"Library: {lib.Name} {lib.Version}");
+                Console.WriteLine($"Type:    {lib.Type}");
+                Console.WriteLine("Reference paths:");
+
+                foreach (string path in lib.ResolveReferencePaths())
+                {
+                    Console.WriteLine(path);
+                }
+            }
+            //</SnippetPrintLibraries>
+        }
+    }
+}
---
+++ snippets/csharp/Microsoft.Extensions.DependencyModel/CompilationLibrary/Overview/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CompilationLibrarySnippets
+{
+    class Program
+    {
+        static void Main()
+        {
+            CompilationLibrarySnippets.SnippetPrintLibraries();
+        }
+    }
+}
