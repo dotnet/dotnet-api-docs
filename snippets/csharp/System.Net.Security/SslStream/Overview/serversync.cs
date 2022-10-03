2f755704e1fb1a2a0dda8c95723a946fe6442dc0
Radek Zikmund <radekzikmund@microsoft.com>
2022-10-03 17:29:35 +0200

    Unify port on client and server in SslStream examples

--- snippets/csharp/System.Net.Security/SslStream/Overview/serversync.cs
+++ snippets/csharp/System.Net.Security/SslStream/Overview/serversync.cs
@@ -22,7 +22,7 @@ namespace Examples.System.Net
         {
             serverCertificate = X509Certificate.CreateFromCertFile(certificate);
             // Create a TCP/IP (IPv4) socket and listen for incoming connections.
-            TcpListener listener = new TcpListener(IPAddress.Any, 8080);
+            TcpListener listener = new TcpListener(IPAddress.Any, 5000);
             listener.Start();
             while (true)
             {
