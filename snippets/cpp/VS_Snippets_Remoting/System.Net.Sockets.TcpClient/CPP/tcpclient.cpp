973aad439201d1146c2a3b7ccc6c5dce27648a41
Radek Zikmund <radekzikmund@microsoft.com>
2022-10-05 14:46:41 +0200

    Make sure TcpClient is disposed in connect examples

--- snippets/cpp/VS_Snippets_Remoting/System.Net.Sockets.TcpClient/CPP/tcpclient.cpp
+++ snippets/cpp/VS_Snippets_Remoting/System.Net.Sockets.TcpClient/CPP/tcpclient.cpp
@@ -32,6 +32,7 @@ using namespace System::Security::Permissions;
 // <Snippet1>
 void Connect( String^ server, String^ message )
 {
+   TcpClient^ client = nullptr;
    try
    {
       // Create a TcpClient.
@@ -39,14 +40,12 @@ void Connect( String^ server, String^ message )
       // connected to the same address as specified by the server, port
       // combination.
       Int32 port = 13000;
-      TcpClient^ client = gcnew TcpClient( server,port );
+      client = gcnew TcpClient( server,port );
       
       // Translate the passed message into ASCII and store it as a Byte array.
       array<Byte>^data = Text::Encoding::ASCII->GetBytes( message );
       
       // Get a client stream for reading and writing.
-      //  Stream stream = client->GetStream();
-
       NetworkStream^ stream = client->GetStream();
       
       // Send the message to the connected TcpServer. 
@@ -54,7 +53,7 @@ void Connect( String^ server, String^ message )
 
       Console::WriteLine( "Sent: {0}", message );
       
-      // Receive the TcpServer::response.
+      // Receive the server response.
 
       // Buffer to store the response bytes.
       data = gcnew array<Byte>(256);
@@ -67,8 +66,10 @@ void Connect( String^ server, String^ message )
       responseData = Text::Encoding::ASCII->GetString( data, 0, bytes );
       Console::WriteLine( "Received: {0}", responseData );
       
-      // Close everything.
-      client->Close();
+      // Explicit close is not necessary since TcpClient::Dispose() will be
+      // called automatically in finally block.
+      // stream->Close();
+      // client->Close();
    }
    catch ( ArgumentNullException^ e ) 
    {
@@ -77,6 +78,11 @@ void Connect( String^ server, String^ message )
    catch ( SocketException^ e ) 
    {
       Console::WriteLine( "SocketException: {0}", e );
+   }
+   finally
+   {
+      if (client != nullptr)
+         delete client;
    }
 
    Console::WriteLine( "\n Press Enter to continue..." );
