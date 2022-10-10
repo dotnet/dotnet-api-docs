2462f54366f7e1b47025c68edba2820f316de3e8
Radek Zikmund <radekzikmund@microsoft.com>
2022-10-10 15:10:00 +0200

    Fix compilation

--- snippets/csharp/System.Net.Security/ALPN/alpn.cs
+++ snippets/csharp/System.Net.Security/ALPN/alpn.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Security;
 using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Threading.Tasks;
 
 async Task RunServer()
 {
