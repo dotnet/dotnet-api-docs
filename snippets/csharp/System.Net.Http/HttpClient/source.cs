ef46e1a54039d76be0e384c7b66769efab1b5b5a
Radek Zikmund <radekzikmund@microsoft.com>
2022-10-17 15:02:12 +0200

    Add examples to HttpResponseMessage.

--- snippets/csharp/System.Net.Http/HttpClient/source.cs
+++ snippets/csharp/System.Net.Http/HttpClient/source.cs
@@ -6,16 +6,16 @@ using System.Threading.Tasks;
 
 class HttpClient_Example
 {
-// <Snippet1>
+    // <Snippet1>
     // HttpClient is intended to be instantiated once per application, rather than per-use. See Remarks.
     static readonly HttpClient client = new HttpClient();
 
     static async Task Main()
     {
         // Call asynchronous network methods in a try/catch block to handle exceptions.
-        try	
+        try
         {
-            HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
+            using HttpResponseMessage response = await client.GetAsync("http://www.contoso.com/");
             response.EnsureSuccessStatusCode();
             string responseBody = await response.Content.ReadAsStringAsync();
             // Above three lines can be replaced with new helper method below
@@ -23,11 +23,11 @@ class HttpClient_Example
 
             Console.WriteLine(responseBody);
         }
-        catch(HttpRequestException e)
+        catch (HttpRequestException e)
         {
-            Console.WriteLine("\nException Caught!");	
-            Console.WriteLine("Message :{0} ",e.Message);
+            Console.WriteLine("\nException Caught!");
+            Console.WriteLine("Message :{0} ", e.Message);
         }
     }
-// </Snippet1>			
+    // </Snippet1>			
 }
