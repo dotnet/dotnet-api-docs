2d2522781930d8ce6a21b0a05e50451775935598
Radek Zikmund <radekzikmund@microsoft.com>
2022-10-17 14:40:54 +0200

    Improve WebResponse.GtResponseStream example

--- snippets/visualbasic/VS_Snippets_Remoting/WebResponse_GetResponseStream/VB/webresponse_getresponsestream.vb
+++ snippets/visualbasic/VS_Snippets_Remoting/WebResponse_GetResponseStream/VB/webresponse_getresponsestream.vb
@@ -43,36 +43,31 @@ Class WebResponseSnippet
             Dim myWebRequest As WebRequest = WebRequest.Create("www.contoso.com")
 
             ' Send the 'WebRequest' and wait for response.
-            Dim myWebResponse As WebResponse = myWebRequest.GetResponse()
+            Using myWebResponse As WebResponse = myWebRequest.GetResponse()
 
-            ' Call method 'GetResponseStream' to obtain stream associated with the response object
-            Dim ReceiveStream As Stream = myWebResponse.GetResponseStream()
-            
-            Dim encode As Encoding = System.Text.Encoding.GetEncoding("utf-8")
+                ' Call method 'GetResponseStream' to obtain stream associated with the response object
+                Dim ReceiveStream As Stream = myWebResponse.GetResponseStream()
+                
+                Dim encode As Encoding = System.Text.Encoding.GetEncoding("utf-8")
 
-            ' Pipe the stream to a higher level stream reader with the required encoding format.
-            Dim readStream As New StreamReader(ReceiveStream, encode)
-            Console.WriteLine(ControlChars.Cr + "Response stream received")
-            Dim read(256) As [Char]
+                ' Pipe the stream to a higher level stream reader with the required encoding format.
+                Dim readStream As New StreamReader(ReceiveStream, encode)
+                Console.WriteLine(ControlChars.Cr + "Response stream received")
+                Dim read(256) As [Char]
 
-            ' Read 256 charcters at a time    .
-            Dim count As Integer = readStream.Read(read, 0, 256)
-            Console.WriteLine("HTML..." + ControlChars.Lf + ControlChars.Cr)
-            While count > 0
+                ' Read 256 charcters at a time    .
+                Dim count As Integer = readStream.Read(read, 0, 256)
+                Console.WriteLine("HTML..." + ControlChars.Lf + ControlChars.Cr)
+                While count > 0
 
-                ' Dump the 256 characters on a string and display the string onto the console.
-                Dim str As New [String](read, 0, count)
-                Console.Write(str)
-                count = readStream.Read(read, 0, 256)
+                    ' Dump the 256 characters on a string and display the string onto the console.
+                    Dim str As New [String](read, 0, count)
+                    Console.Write(str)
+                    count = readStream.Read(read, 0, 256)
 
-            End While
-            Console.WriteLine("")
-
-            ' Release the resources of stream object.
-	         readStream.Close()
-
-	         ' Release the resources of response object.
-            myWebResponse.Close()
+                End While
+                Console.WriteLine("")
+            End Using
             
 ' </Snippet1>        
         Catch e As WebException
