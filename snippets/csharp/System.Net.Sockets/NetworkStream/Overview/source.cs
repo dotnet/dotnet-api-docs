c04218ff5332d93a3145a46afcf6541224a1e824
Anton Firszov <antonfir@gmail.com>
2022-10-19 22:15:48 +0200

    better example for NetworkStream.Read()

--- snippets/csharp/System.Net.Sockets/NetworkStream/Overview/source.cs
+++ snippets/csharp/System.Net.Sockets/NetworkStream/Overview/source.cs
@@ -57,33 +57,23 @@ public class NetworkStream_Sync_Send_Receive
 
             //</Snippet3>
 
-            //<Snippet4>
             // Examples for Read.
+            //<Snippet4>
 
-            // Check to see if this NetworkStream is readable.
-            if (myNetworkStream.CanRead)
+            byte[] myReadBuffer = new byte[1024];
+            StringBuilder myCompleteMessage = new StringBuilder();
+            int numberOfBytesRead = myNetworkStream.Read(myReadBuffer, 0, myReadBuffer.Length);
+
+            // Read all the data until the end of stream has been reached.
+            // The incoming message may be larger than the buffer size.
+            while (numberOfBytesRead > 0)
             {
-                byte[] myReadBuffer = new byte[1024];
-                StringBuilder myCompleteMessage = new StringBuilder();
-                int numberOfBytesRead = 0;
-
-                // The incoming message may be larger than the buffer size.
-                do
-                {
-                    numberOfBytesRead = myNetworkStream.Read(myReadBuffer, 0, myReadBuffer.Length);
-
-                    myCompleteMessage.AppendFormat("{0}", Encoding.ASCII.GetString(myReadBuffer, 0, numberOfBytesRead));
-                }
-                while (myNetworkStream.DataAvailable);
-
-                // Print out the received message to the console.
-                Console.WriteLine("You received the following message : " +
-                                             myCompleteMessage);
+                myCompleteMessage.Append(Encoding.ASCII.GetString(myReadBuffer, 0, numberOfBytesRead));
+                numberOfBytesRead = myNetworkStream.Read(myReadBuffer, 0, myReadBuffer.Length);
             }
-            else
-            {
-                Console.WriteLine("Sorry.  You cannot read from this NetworkStream.");
-            }
+
+            // Print out the received message to the console.
+            Console.WriteLine("You received the following message : " + myCompleteMessage);
 
             //</Snippet4>
 
@@ -105,6 +95,7 @@ public class NetworkStream_Sync_Send_Receive
 
     public static void Main(String[] args)
     {
+        MySample(true);
         if (args[0] == "yes")
         {
             NetworkStream_Sync_Send_Receive.MySample(true);
