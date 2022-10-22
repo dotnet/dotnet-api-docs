1453f9ba5515e9f12eea121dfe9b774f0c1c6957
softwarepronto <jann@softwarepronto.com>
2022-10-22 05:10:43 -0700

    Missing using for StreamReader variable
    The example includes:
    using WebClient client = new WebClient();
    using Stream data = client.OpenRead(args[0]);
    
    But was missing:
    StreamReader reader = new StreamReader(data);

--- snippets/csharp/System.Net/WebClient/Overview/useragent.cs
+++ snippets/csharp/System.Net/WebClient/Overview/useragent.cs
@@ -19,7 +19,7 @@ public class Test
         client.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
 
         using Stream data = client.OpenRead(args[0]);
-        StreamReader reader = new StreamReader(data);
+        using StreamReader reader = new StreamReader(data);
         string s = reader.ReadToEnd();
         Console.WriteLine(s);
         data.Close();
