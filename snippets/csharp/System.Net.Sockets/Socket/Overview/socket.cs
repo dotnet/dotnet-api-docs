5d503b24f2a9ed44d32c59463d27b9e9718ad0a7
Anton Firszov <antonfir@gmail.com>
2022-10-31 20:45:58 +0100

    avoid indentation of example methods

--- snippets/csharp/System.Net.Sockets/Socket/Overview/socket.cs
+++ snippets/csharp/System.Net.Sockets/Socket/Overview/socket.cs
@@ -25,85 +25,85 @@ public class GetSocket
         await SendHttpRequestAsync(uri);
     }
 
-    //<Snippet1>
-    private static void SendHttpRequest(Uri? uri = null, int port = 80)
-    {
-        uri ??= new Uri("http://example.com");
+//<Snippet1>
+private static void SendHttpRequest(Uri? uri = null, int port = 80)
+{
+    uri ??= new Uri("http://example.com");
 
-        // Construct a minimalistic HTTP/1.1 request
-        byte[] requestBytes = Encoding.ASCII.GetBytes(@$"GET {uri.AbsoluteUri} HTTP/1.1
+    // Construct a minimalistic HTTP/1.1 request
+    byte[] requestBytes = Encoding.ASCII.GetBytes(@$"GET {uri.AbsoluteUri} HTTP/1.1
 Host: {uri.Host}
 Connection: Close
 
 ");
 
-        // Create and connect a dual-stack socket
-        using Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-        socket.Connect(uri.Host, port);
+    // Create and connect a dual-stack socket
+    using Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+    socket.Connect(uri.Host, port);
 
-        // Send the request
-        int sendCnt = socket.Send(requestBytes);
+    // Send the request
+    int sendCnt = socket.Send(requestBytes);
 
-        // Do minimalistic buffering assuming ASCII response
-        byte[] responseBytes = new byte[256];
-        char[] responseChars = new char[256];
+    // Do minimalistic buffering assuming ASCII response
+    byte[] responseBytes = new byte[256];
+    char[] responseChars = new char[256];
 
-        while (true)
-        {
-            int byteCount = socket.Receive(responseBytes);
-
-            // Receiving 0 bytes means EOF has been reached
-            if (byteCount == 0) break;
-
-            // Convert byteCount bytes to ASCII characters using the 'responseChars' buffer as destination
-            int charCount = Encoding.ASCII.GetChars(responseBytes, 0, byteCount, responseChars, 0);
-
-            // Print the contents of the 'responseChars' buffer to Console.Out
-            Console.Out.Write(responseChars, 0, charCount);
-        }
-
-        Console.Out.Flush();
-    }
-    //</Snippet1>
-
-    //<Snippet2>
-    private static async Task SendHttpRequestAsync(Uri? uri = null, int port = 80, CancellationToken cancellationToken = default)
+    while (true)
     {
-        uri ??= new Uri("http://example.com");
+        int byteCount = socket.Receive(responseBytes);
 
-        // Construct a minimalistic HTTP/1.1 request
-        byte[] requestBytes = Encoding.ASCII.GetBytes(@$"GET {uri.AbsoluteUri} HTTP/1.1
+        // Receiving 0 bytes means EOF has been reached
+        if (byteCount == 0) break;
+
+        // Convert byteCount bytes to ASCII characters using the 'responseChars' buffer as destination
+        int charCount = Encoding.ASCII.GetChars(responseBytes, 0, byteCount, responseChars, 0);
+
+        // Print the contents of the 'responseChars' buffer to Console.Out
+        Console.Out.Write(responseChars, 0, charCount);
+    }
+
+    Console.Out.Flush();
+}
+//</Snippet1>
+
+//<Snippet2>
+private static async Task SendHttpRequestAsync(Uri? uri = null, int port = 80, CancellationToken cancellationToken = default)
+{
+    uri ??= new Uri("http://example.com");
+
+    // Construct a minimalistic HTTP/1.1 request
+    byte[] requestBytes = Encoding.ASCII.GetBytes(@$"GET {uri.AbsoluteUri} HTTP/1.1
 Host: {uri.Host}
 Connection: Close
 
 ");
 
-        // Create and connect a dual-stack socket
-        using Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
-        await socket.ConnectAsync(uri.Host, port, cancellationToken);
+    // Create and connect a dual-stack socket
+    using Socket socket = new Socket(SocketType.Stream, ProtocolType.Tcp);
+    await socket.ConnectAsync(uri.Host, port, cancellationToken);
 
-        // Send the request
-        int sendCnt = await socket.SendAsync(requestBytes, SocketFlags.None, cancellationToken);
+    // Send the request
+    int sendCnt = await socket.SendAsync(requestBytes, SocketFlags.None, cancellationToken);
 
-        // Do minimalistic buffering assuming ASCII response
-        byte[] responseBytes = new byte[256];
-        char[] responseChars = new char[256];
+    // Do minimalistic buffering assuming ASCII response
+    byte[] responseBytes = new byte[256];
+    char[] responseChars = new char[256];
 
-        while (true)
-        {
-            int byteCount = await socket.ReceiveAsync(responseBytes, SocketFlags.None, cancellationToken);
+    while (true)
+    {
+        int byteCount = await socket.ReceiveAsync(responseBytes, SocketFlags.None, cancellationToken);
 
-            // Receiving 0 bytes means EOF has been reached
-            if (byteCount == 0) break;
+        // Receiving 0 bytes means EOF has been reached
+        if (byteCount == 0) break;
 
-            // Convert byteCount bytes to ASCII characters using the 'responseChars' buffer as destination
-            int charCount = Encoding.ASCII.GetChars(responseBytes, 0, byteCount, responseChars, 0);
+        // Convert byteCount bytes to ASCII characters using the 'responseChars' buffer as destination
+        int charCount = Encoding.ASCII.GetChars(responseBytes, 0, byteCount, responseChars, 0);
 
-            // Print the contents of the 'responseChars' buffer to Console.Out
-            await Console.Out.WriteAsync(responseChars.AsMemory(0, charCount), cancellationToken);
-        }
-
-        await Console.Out.FlushAsync();
+        // Print the contents of the 'responseChars' buffer to Console.Out
+        await Console.Out.WriteAsync(responseChars.AsMemory(0, charCount), cancellationToken);
     }
-    //</Snippet2>
+
+    await Console.Out.FlushAsync();
+}
+//</Snippet2>
 }
