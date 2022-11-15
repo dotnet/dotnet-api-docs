abf4ecb57d720a14178d6c6dad019790ffc8cb62
Albert Du <52804499+albert-du@users.noreply.github.com>
2022-11-15 07:15:48 -0800

    System.IO.File F# snippets (#8627)
    * File F# snippets
    
    * fix build

---
+++ snippets/fsharp/System.IO/File/Replace/sample.fs
@@ -0,0 +1,20 @@
+﻿//<SNIPPET1>
+open System
+open System.IO
+
+// Move a file into another file, delete the original, and create a backup of the replaced file.
+let replaceFile fileToMoveAndDelete fileToReplace backupOfFileToReplace =
+    File.Replace(fileToMoveAndDelete, fileToReplace, backupOfFileToReplace, false)
+
+let originalFile = "test.xml"
+let fileToReplace = "test2.xml"
+let backUpOfFileToReplace = "test2.xml.bac"
+
+printfn
+    $"Move the contents of {originalFile} into {fileToReplace}, delete {originalFile}, and create a backup of {fileToReplace}."
+
+// Replace the file.
+replaceFile originalFile fileToReplace backUpOfFileToReplace
+
+printfn "Done"
+//</SNIPPET1>
