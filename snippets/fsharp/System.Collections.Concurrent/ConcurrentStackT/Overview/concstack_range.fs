52128d686e81dfacdec4181647138e3bca9f1ba7
Albert Du <52804499+albert-du@users.noreply.github.com>
2023-02-20 20:04:56 -0800

    ConcurrentStack<T> F# snippets

---
+++ snippets/fsharp/System.Collections.Concurrent/ConcurrentStackT/Overview/concstack_range.fs
@@ -0,0 +1,51 @@
+﻿module concstack_range
+//<snippet1>
+open System.Collections.Concurrent
+open System.Linq
+open System.Threading
+open System.Threading.Tasks
+
+// Demonstrates:
+//      ConcurrentStack<T>.PushRange();
+//      ConcurrentStack<T>.TryPopRange();
+
+let main = 
+    task {
+        let numParallelTasks = 4
+        let numItems = 1000
+        let stack = ConcurrentStack<int>()
+
+        // Push a range of values onto the stack concurrently
+        let! _ = Task.WhenAll(Enumerable.Range(0, numParallelTasks).Select(fun i -> 
+            Task.Factory.StartNew((fun state ->
+                // state = i * numItems
+                let index: int = unbox state
+                let array = 
+                    [|  for j in 0 .. numItems - 1 do
+                            index + j |]
+                printfn $"Pushing an array of ints from {array[0]} to {array[numItems - 1]}"
+                stack.PushRange array
+            ), i * numItems, CancellationToken.None, TaskCreationOptions.DenyChildAttach, TaskScheduler.Default)).ToArray())
+
+        let numTotalElements = 4 * numItems
+        let resultBuffer = Array.zeroCreate numTotalElements
+        let! _ = Task.WhenAll(Enumerable.Range(0, numParallelTasks).Select(fun i -> 
+            Task.Factory.StartNew((fun obj ->
+                let index = unbox obj
+                let result = stack.TryPopRange(resultBuffer, index, numItems)
+                printfn $"TryPopRange expected {numItems}, got {result}."
+            ), i * numItems, CancellationToken.None, TaskCreationOptions.LongRunning, TaskScheduler.Default)).ToArray())
+
+        for i = 0 to numParallelTasks - 1 do
+            // Create a sequence we expect to see from the stack taking the last number of the range we inserted
+            let expected = Enumerable.Range(resultBuffer[i*numItems + numItems - 1], numItems)
+
+            // Take the range we inserted, reverse it, and compare to the expected sequence
+            let areEqual = expected.SequenceEqual(resultBuffer.Skip(i * numItems).Take(numItems).Reverse())
+            if areEqual then
+                printfn $"Expected a range of {expected.First()} to {expected.Last()}. Got {resultBuffer[i * numItems + numItems - 1]} to {resultBuffer[i * numItems]}"
+            else
+                printfn $"Unexpected consecutive ranges."
+    }
+main.Wait()
+//</snippet1>
---
+++ snippets/fsharp/System.Collections.Concurrent/ConcurrentStackT/Overview/concstack_single.fs
@@ -0,0 +1,67 @@
+﻿module concstack_single
+//<snippet1>
+open System
+open System.Collections.Concurrent
+open System.Threading.Tasks
+
+// Demonstrates:
+//      ConcurrentStack<T>.Push();
+//      ConcurrentStack<T>.TryPeek();
+//      ConcurrentStack<T>.TryPop();
+//      ConcurrentStack<T>.Clear();
+//      ConcurrentStack<T>.IsEmpty;
+
+let main =
+    task {
+        let items = 10000
+        let stack = ConcurrentStack<int>()
+
+        // Create an action to push items onto the stack
+        let pusher =
+            Action(fun () ->
+                for i = 0 to items - 1 do
+                    stack.Push i)
+
+        // Run the action once
+        pusher.Invoke()
+        let mutable result = 0
+
+        if stack.TryPeek &result then
+            printfn $"TryPeek() saw {result} on top of the stack."
+        else
+            printfn "Could not peek most recently added number."
+
+        // Empty the stack
+        stack.Clear()
+
+        if stack.IsEmpty then
+            printfn "Cleared the stack."
+
+        // Create an action to push and pop items
+        let pushAndPop =
+            Action(fun () ->
+                printfn $"Task started on {Task.CurrentId}"
+
+                let mutable item = 0
+
+                for i = 0 to items - 1 do
+                    stack.Push i
+
+                for i = 0 to items - 1 do
+                    stack.TryPop &item |> ignore
+
+                printfn $"Task ended on {Task.CurrentId}")
+        // Spin up five concurrent tasks of the action
+        let tasks =
+            [| for i = 0 to 4 do
+                   Task.Run pushAndPop |]
+
+        // Wait for all the tasks to finish up
+        do! Task.WhenAll tasks
+
+        if not stack.IsEmpty then
+            printfn "Did not take all the items off the stack"
+    }
+
+main.Wait()
+//</snippet1>
