1eea25dfb318d65695e8da7a4f51ba6423deb71d
Albert Du <52804499+albert-du@users.noreply.github.com>
2023-02-24 22:54:54 -0800

    Task F# snippets

---
+++ snippets/fsharp/System.Threading.Tasks/Task/.ctor/Run11.fs
@@ -0,0 +1,17 @@
+﻿module Run11
+// <Snippet11>
+open System.Threading
+open System.Threading.Tasks
+
+let showThreadInfo s =
+    printfn $"%s{s} thread ID: {Thread.CurrentThread.ManagedThreadId}"
+
+showThreadInfo "Application"
+
+let t = Task.Run(fun () -> showThreadInfo "Task")
+t.Wait()
+
+// The example displays the following output:
+//       Application thread ID: 1
+//       Task thread ID: 3
+// </Snippet11>
