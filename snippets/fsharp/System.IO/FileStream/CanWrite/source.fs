fcec45577d6ceea28695e5395688b02d0935c884
Albert Du <52804499+albert-du@users.noreply.github.com>
2023-02-26 12:52:54 -0800

    FileStream F# snippets

---
+++ snippets/fsharp/System.IO/FileStream/CanWrite/source.fs
@@ -0,0 +1,12 @@
+﻿module source
+// <Snippet1>
+open System.IO
+
+let fs = new FileStream("MyFile.txt", FileMode.OpenOrCreate, FileAccess.Write)
+
+if fs.CanRead && fs.CanWrite then
+    printfn "MyFile.txt can be both written to and read from."
+elif fs.CanWrite then
+    printfn "MyFile.txt is writable."
+
+// </Snippet1>
---
+++ snippets/fsharp/System.IO/FileStream/Length/source.fs
@@ -0,0 +1,7 @@
+﻿module source
+
+let func (s: System.IO.FileStream) =
+    // <Snippet1>
+    if s.Length = s.Position then
+        printfn "End of file has been reached."
+// </Snippet1>
