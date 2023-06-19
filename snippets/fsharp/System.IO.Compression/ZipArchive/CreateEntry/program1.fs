5ee65f8c1edac06675a7cad9ab96701c210e4bf2
Albert Du <52804499+albert-du@users.noreply.github.com>
2023-06-19 07:14:30 -0700

    System.IO.Compression F# snippets (#9065)
    * DeflateStream
    
    * GZipStream
    
    * ZipArchive
    
    * ZipFile

---
+++ snippets/fsharp/System.IO.Compression/ZipArchive/CreateEntry/program1.fs
@@ -0,0 +1,17 @@
+﻿module program1
+
+// <snippet1>
+open System.IO
+open System.IO.Compression
+
+do
+    use zipToOpen = new FileStream(@"c:\users\exampleuser\release.zip", FileMode.Open)
+
+    use archive = new ZipArchive(zipToOpen, ZipArchiveMode.Update)
+
+    let readmeEntry = archive.CreateEntry "Readme.txt"
+    use writer = new StreamWriter(readmeEntry.Open())
+
+    writer.WriteLine "Information about this package."
+    writer.WriteLine "========================"
+// </snippet1>
