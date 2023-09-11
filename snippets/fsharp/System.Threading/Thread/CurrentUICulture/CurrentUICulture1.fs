675f89f1fec12114681715aba074388ed23143b8
Albert Du <52804499+albert-du@users.noreply.github.com>
2023-09-11 07:31:56 -0700

    System.Threading.Thread F# snippets (#8862)
    * Thread F# snippets
    
    * fix snippet tag?

---
+++ snippets/fsharp/System.Threading/Thread/CurrentUICulture/CurrentUICulture1.fs
@@ -0,0 +1,23 @@
+﻿// <Snippet1>
+open System.Globalization
+open System.Threading
+
+// Change the current culture if the language is not French.
+let current = Thread.CurrentThread.CurrentUICulture
+
+if current.TwoLetterISOLanguageName <> "fr" then
+    let newCulture = CultureInfo.CreateSpecificCulture "en-US"
+    Thread.CurrentThread.CurrentUICulture <- newCulture
+    // Make current UI culture consistent with current culture.
+    Thread.CurrentThread.CurrentCulture <- newCulture
+
+printfn
+    $"The current UI culture is {Thread.CurrentThread.CurrentUICulture.NativeName} [{Thread.CurrentThread.CurrentUICulture.Name}]"
+
+printfn
+    $"The current culture is {Thread.CurrentThread.CurrentUICulture.NativeName} [{Thread.CurrentThread.CurrentUICulture.Name}]"
+
+// The example displays the following output:
+//     The current UI culture is English (United States) [en-US]
+//     The current culture is English (United States) [en-US]
+// </Snippet1>
---
+++ snippets/fsharp/System.Threading/Thread/GetApartmentState/source.fs
@@ -0,0 +1,31 @@
+﻿//<Snippet1>
+open System.Threading
+
+let threadProc () = Thread.Sleep 2000
+
+let t = Thread threadProc
+printfn $"Before setting apartment state: {t.GetApartmentState()}"
+
+t.SetApartmentState ApartmentState.STA
+printfn $"After setting apartment state: {t.GetApartmentState()}"
+
+let result = t.TrySetApartmentState ApartmentState.MTA
+printfn $"Try to change state: {result}"
+
+t.Start()
+
+Thread.Sleep 500
+
+try
+    t.TrySetApartmentState ApartmentState.STA |> ignore
+with :? ThreadStateException ->
+    printfn "ThreadStateException occurs if apartment state is set after starting thread."
+
+t.Join()
+
+// This code example produces the following output:
+//     Before setting apartment state: Unknown
+//     After setting apartment state: STA
+//     Try to change state: False
+//     ThreadStateException occurs if apartment state is set after starting thread.
+//</Snippet1>
