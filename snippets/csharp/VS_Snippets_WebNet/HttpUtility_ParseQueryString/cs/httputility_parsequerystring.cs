61727aebb1cf75bf28796a0476e72da9177aa23a
Joan Gil <joan.gil@convidad.es>
2023-11-08 09:59:13 +0100

    Update c# snippet to wrap it inside a Program class

--- snippets/csharp/VS_Snippets_WebNet/HttpUtility_ParseQueryString/cs/httputility_parsequerystring.cs
+++ snippets/csharp/VS_Snippets_WebNet/HttpUtility_ParseQueryString/cs/httputility_parsequerystring.cs
@@ -1,18 +1,25 @@
 // <Snippet1>
 
+using System;
 using System.Web;
 
-// Parse the url and get the query string
-var url = "https://www.microsoft.com?name=John&age=30&location=USA";
-var parsedUrl = url.Split('?')[1];
-
-// The ParseQueryString method will parse the query string and return a NameValueCollection
-var paramsCollection = HttpUtility.ParseQueryString(parsedUrl);
-
-// The for each loop will iterate over the params collection and print the key and value for each param
-foreach(var key in paramsCollection.AllKeys)
+class Program
 {
-    Console.WriteLine($"Key: {key} => Value: {paramsCollection[key]}");
+    static void Main()
+    {
+        // Parse the URL and get the query string
+        var url = "https://www.microsoft.com?name=John&age=30&location=USA";
+        var parsedUrl = url.Split('?')[1];
+
+        // The ParseQueryString method will parse the query string and return a NameValueCollection
+        var paramsCollection = HttpUtility.ParseQueryString(parsedUrl);
+
+        // The foreach loop will iterate over the params collection and print the key and value for each param
+        foreach (var key in paramsCollection.AllKeys)
+        {
+            Console.WriteLine($"Key: {key} => Value: {paramsCollection[key]}");
+        }
+    }
 }
 
 // The example displays the following output:
