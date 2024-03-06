ac828a3ab110fff3cde4c140e1a4cb4973b4a8e3
ogmini <112595633+ogmini@users.noreply.github.com>
2024-03-06 15:08:19 -0500

    Update source.cs
    Fixing using statement

--- snippets/csharp/System.IO/FileSystemWatcher/Overview/source.cs
+++ snippets/csharp/System.IO/FileSystemWatcher/Overview/source.cs
@@ -8,29 +8,30 @@ namespace MyNamespace
     {
         static void Main()
         {
-            using var watcher = new FileSystemWatcher(@"C:\path\to\folder");
-
-            watcher.NotifyFilter = NotifyFilters.Attributes
-                                 | NotifyFilters.CreationTime
-                                 | NotifyFilters.DirectoryName
-                                 | NotifyFilters.FileName
-                                 | NotifyFilters.LastAccess
-                                 | NotifyFilters.LastWrite
-                                 | NotifyFilters.Security
-                                 | NotifyFilters.Size;
-
-            watcher.Changed += OnChanged;
-            watcher.Created += OnCreated;
-            watcher.Deleted += OnDeleted;
-            watcher.Renamed += OnRenamed;
-            watcher.Error += OnError;
-
-            watcher.Filter = "*.txt";
-            watcher.IncludeSubdirectories = true;
-            watcher.EnableRaisingEvents = true;
-
-            Console.WriteLine("Press enter to exit.");
-            Console.ReadLine();
+            using (var watcher = new FileSystemWatcher(@"C:\path\to\folder"))
+            {
+                watcher.NotifyFilter = NotifyFilters.Attributes
+                                     | NotifyFilters.CreationTime
+                                     | NotifyFilters.DirectoryName
+                                     | NotifyFilters.FileName
+                                     | NotifyFilters.LastAccess
+                                     | NotifyFilters.LastWrite
+                                     | NotifyFilters.Security
+                                     | NotifyFilters.Size;
+    
+                watcher.Changed += OnChanged;
+                watcher.Created += OnCreated;
+                watcher.Deleted += OnDeleted;
+                watcher.Renamed += OnRenamed;
+                watcher.Error += OnError;
+    
+                watcher.Filter = "*.txt";
+                watcher.IncludeSubdirectories = true;
+                watcher.EnableRaisingEvents = true;
+    
+                Console.WriteLine("Press enter to exit.");
+                Console.ReadLine();
+            }
         }
 
         private static void OnChanged(object sender, FileSystemEventArgs e)
