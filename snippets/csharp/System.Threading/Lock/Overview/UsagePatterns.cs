8aa1894ab523063e7ef7ec0dc42cf1ab1a0c638e
Koundinya Veluri <kouvel@microsoft.com>
2024-05-21 19:44:41 -0700

    Add code snippet for System.Threading.Lock
    This reverts commit 6d46e61a411c85e6b5a643d293bbc0e86997d5d0.

---
+++ snippets/csharp/System.Threading/Lock/Overview/UsagePatterns.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+// <Snippet1>
+public sealed class ExampleDataStructure
+{
+    private readonly Lock _lockObj = new();
+
+    private void Modify()
+    {
+        lock (_lockObj)
+        {
+            // Critical section associated with _lockObj
+        }
+
+        using (_lockObj.EnterScope())
+        {
+            // Critical section associated with _lockObj
+        }
+
+        _lockObj.Enter();
+        try
+        {
+            // Critical section associated with _lockObj
+        }
+        finally { _lockObj.Exit(); }
+
+        if (_lockObj.TryEnter())
+        {
+            try
+            {
+                // Critical section associated with _lockObj
+            }
+            finally { _lockObj.Exit(); }
+        }
+    }
+}
+// </Snippet1>
