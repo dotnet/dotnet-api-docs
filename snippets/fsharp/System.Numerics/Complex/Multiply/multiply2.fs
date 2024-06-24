497baea73c31ad56203a2d1bbb96d4ce17867fec
Albert Du <52804499+albert-du@users.noreply.github.com>
2024-06-24 07:23:48 -0700

    F# snippets for Complex (#10052)

---
+++ snippets/fsharp/System.Numerics/Complex/Multiply/multiply2.fs
@@ -0,0 +1,9 @@
+﻿module multiply2
+
+open System.Numerics
+
+// <Snippet2>
+let c1 = Complex.One
+let c2 = Complex(1.4, 2.3)
+let c3 = c1 * c2
+// </Snippet2>
---
+++ snippets/fsharp/System.Numerics/Complex/ToString/tostring2.fs
@@ -0,0 +1,24 @@
+﻿module tostring2
+// <Snippet2>
+open System.Globalization
+open System.Numerics
+
+let c =
+    [ Complex(17.3, 14.1)
+      Complex(-18.9, 147.2)
+      Complex(13.472, -18.115)
+      Complex(-11.154, -17.002) ]
+
+let cultures = [ CultureInfo "en-US"; CultureInfo "fr-FR" ]
+
+for c1 in c do
+    for culture in cultures do
+        printf $"{c1.ToString culture} ({culture.Name})"
+
+    printfn ""
+// The example displays the following output:
+//       (17.3, 14.1) (en-US)    (17,3, 14,1) (fr-FR)
+//       (-18.9, 147.2) (en-US)    (-18,9, 147,2) (fr-FR)
+//       (13.472, -18.115) (en-US)    (13,472, -18,115) (fr-FR)
+//       (-11.154, -17.002) (en-US)    (-11,154, -17,002) (fr-FR)
+// </Snippet2>
