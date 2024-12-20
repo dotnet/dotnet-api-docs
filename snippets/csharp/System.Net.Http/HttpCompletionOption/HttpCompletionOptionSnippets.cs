1a44c31788839a41ecd827eebab9b6fad830f705
FERNAN OVIEDO CANDELARESI <feoviedo@microsoft.com>
2024-12-20 11:38:58 -0800

    Move code sample to standalone snippet

---
+++ snippets/csharp/System.Net.Http/HttpCompletionOption/HttpCompletionOptionSnippets.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace HttpCompletionOptionSnippets
+{
+    class HttpCompletionOptionSnippets
+    {
+        public static async Task RunAsync()
+        {
+            //<SnippetHttpCompletionOption>
+            var httpClient = new HttpClient();
+            httpClient.Timeout = TimeSpan.FromSeconds(30);
+            using (var response = await httpClient.GetAsync("http://localhost:12345/", HttpCompletionOption.ResponseHeadersRead)) // 30-second timeout but ONLY up until past the headers
+            {
+                // Do other stuff that doesn't rely on the content first, like status code validation
+                response.EnsureSuccessStatusCode();
+
+                var content = await response.Content.ReadAsStringAsync(); // NO TIMEOUT
+            }
+            //</SnippetHttpCompletionOption>
+        }
+    }
+}
