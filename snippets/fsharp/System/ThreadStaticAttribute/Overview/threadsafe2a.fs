0afabb53ec9ad0b91024ef0a35b89b74e7938141
Aaron R Robinson <arobins@microsoft.com>
2025-03-14 23:03:28 -0700

    Feedback

--- snippets/fsharp/System/ThreadStaticAttribute/Overview/threadsafe2a.fs
+++ snippets/fsharp/System/ThreadStaticAttribute/Overview/threadsafe2a.fs
@@ -2,101 +2,31 @@
 open System
 open System.Threading
 
-type Example() =
+type ThreadLocal() =
     [<ThreadStatic; DefaultValue>]
-    static val mutable private previous : double
+    static val mutable private requestId: string option
 
-    [<ThreadStatic; DefaultValue>]
-    static val mutable private sum : double
+    static member PerformLogging() =
+        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Logging request {ThreadLocal.requestId.Value}")
 
-    [<ThreadStatic; DefaultValue>]
-    static val mutable private calls : int
+    static member PerformDatabaseOperation() =
+        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Processing DB operation for request {ThreadLocal.requestId.Value}")
 
-    [<ThreadStatic; DefaultValue>]
-    static val mutable private abnormal : bool
+    static member ProcessRequest(reqId: obj) =
+        ThreadLocal.requestId <- Some(reqId :?> string)
+        ThreadLocal.PerformDatabaseOperation()
+        ThreadLocal.PerformLogging()
 
-    static let mutable totalNumbers = 0
-    static let countdown = new CountdownEvent(1)
-    static let lockObj = obj ()
-    let rand = Random()
+[<EntryPoint>]
+let main _ =
+    let thread1 = Thread(ThreadStart(fun () -> ThreadLocal.ProcessRequest("REQ-001")))
+    let thread2 = Thread(ThreadStart(fun () -> ThreadLocal.ProcessRequest("REQ-002")))
 
+    thread1.Start()
+    thread2.Start()
+    thread1.Join()
+    thread2.Join()
 
-    member this.Execute() =
-        for threads = 1 to 10 do
-            let newThread = new Thread(ThreadStart this.GetRandomNumbers)
-            countdown.AddCount()
-            newThread.Name <- threads.ToString()
-            newThread.Start()
-        this.GetRandomNumbers()
-        countdown.Wait()
-        printfn $"{totalNumbers:N0} random numbers were generated."
-
-    member _.GetRandomNumbers() =
-        // Initialize ThreadStatic fields for this thread.
-        Example.previous <- 0.0
-        Example.sum <- 0.0
-        Example.calls <- 0
-        Example.abnormal <- false
-
-        let mutable i = 0
-        while i < 2000000 do
-            lock lockObj (fun () ->
-                let result = rand.NextDouble()
-                Example.calls <- Example.calls + 1
-                Interlocked.Increment &totalNumbers |> ignore
-                // We should never get the same random number twice.
-                if result = Example.previous then
-                    Example.abnormal <- true
-                    i <- 2000001 // break
-                else
-                    Example.previous <- result
-                    Example.sum <- Example.sum + result )
-            i <- i + 1
-        // get last result
-        if Example.abnormal then
-            printfn $"Result is {Example.previous} in {Thread.CurrentThread.Name}"
-
-        printfn $"Thread {Thread.CurrentThread.Name} finished random number generation."
-        printfn $"Sum = {Example.sum:N4}, Mean = {Example.sum / float Example.calls:N4}, n = {Example.calls:N0}\n"
-        countdown.Signal() |> ignore
-
-let ex = Example()
-Thread.CurrentThread.Name <- "Main"
-ex.Execute()
-
-// The example displays output similar to the following:
-//    Thread 1 finished random number generation.
-//    Sum = 1,000,556.7483, Mean = 0.5003, n = 2,000,000
-//
-//    Thread 6 finished random number generation.
-//    Sum = 999,704.3865, Mean = 0.4999, n = 2,000,000
-//
-//    Thread 2 finished random number generation.
-//    Sum = 999,680.8904, Mean = 0.4998, n = 2,000,000
-//
-//    Thread 10 finished random number generation.
-//    Sum = 999,437.5132, Mean = 0.4997, n = 2,000,000
-//
-//    Thread 8 finished random number generation.
-//    Sum = 1,000,663.7789, Mean = 0.5003, n = 2,000,000
-//
-//    Thread 4 finished random number generation.
-//    Sum = 999,379.5978, Mean = 0.4997, n = 2,000,000
-//
-//    Thread 5 finished random number generation.
-//    Sum = 1,000,011.0605, Mean = 0.5000, n = 2,000,000
-//
-//    Thread 9 finished random number generation.
-//    Sum = 1,000,637.4556, Mean = 0.5003, n = 2,000,000
-//
-//    Thread Main finished random number generation.
-//    Sum = 1,000,676.2381, Mean = 0.5003, n = 2,000,000
-//
-//    Thread 3 finished random number generation.
-//    Sum = 999,951.1025, Mean = 0.5000, n = 2,000,000
-//
-//    Thread 7 finished random number generation.
-//    Sum = 1,000,844.5217, Mean = 0.5004, n = 2,000,000
-//
-//    22,000,000 random numbers were generated.
+    Console.WriteLine("Main thread execution completed.")
+    0
 // </Snippet1>
--- snippets/visualbasic/VS_Snippets_CLR_System/system.threadstaticattribute/vb/threadsafe2a.vb
+++ snippets/visualbasic/VS_Snippets_CLR_System/system.threadstaticattribute/vb/threadsafe2a.vb
@@ -2,110 +2,43 @@
 Option Strict On
 
 ' <Snippet1>
+Imports System
 Imports System.Threading
 
-Public Class Example
-   <ThreadStatic> Shared previous As Double
-   <ThreadStatic> Shared sum As Double
-   <ThreadStatic> Shared calls As Integer
-   <ThreadStatic> Shared abnormal As Boolean
-   Shared totalNumbers As Integer = 0
-   Shared countdown As CountdownEvent
-   Private Shared lockObj As Object
-   Dim rand As Random
+Module Program
 
-   Public Sub New()
-      rand = New Random()
-      lockObj = New Object()
-      countdown = New CountdownEvent(1)
-   End Sub
+    <ThreadStatic>
+    Private _requestId As String
 
-   Public Shared Sub Main()
-      Dim ex As New Example()
-      Thread.CurrentThread.Name = "Main"
-      ex.Execute()
-      countdown.Wait()
-      Console.WriteLine("{0:N0} random numbers were generated.", totalNumbers)
-   End Sub
+    Sub Main()
+        Dim thread1 As New Thread(AddressOf ProcessRequest)
+        Dim thread2 As New Thread(AddressOf ProcessRequest)
 
-   Private Sub Execute()
-      For threads As Integer = 1 To 10
-         Dim newThread As New Thread(New ThreadStart(AddressOf GetRandomNumbers))
-         countdown.AddCount()
-         newThread.Name = threads.ToString()
-         newThread.Start()
-      Next
-      Me.GetRandomNumbers()
-   End Sub
+        thread1.Start("REQ-001")
+        thread2.Start("REQ-002")
 
-   Private Sub GetRandomNumbers()
-      ' Initialize ThreadStatic fields for this thread.
-      previous = 0.0
-      sum = 0.0
-      calls = 0
-      abnormal = False
+        thread1.Join()
+        thread2.Join()
 
-      Dim result As Double = 0.0
+        Console.WriteLine("Main thread execution completed.")
+    End Sub
 
-      For ctr As Integer = 1 To 2000000
-         SyncLock lockObj
-            result = rand.NextDouble()
-            calls += 1
-            Interlocked.Increment(totalNumbers)
-            ' We should never get the same random number twice.
-            If result = previous Then
-               abnormal = True
-               Exit For
-            Else
-               previous = result
-               sum += result
-            End If
-         End SyncLock
-      Next
-      ' Get last result.
-      If abnormal Then
-         Console.WriteLine("Result is {0} in {1}", previous, Thread.CurrentThread.Name)
-      End If
+    Sub ProcessRequest(ByVal requestId As Object)
+        ' Assign the request ID to the thread-static field
+        _requestId = requestId.ToString()
 
-      Console.WriteLine("Thread {0} finished random number generation.", Thread.CurrentThread.Name)
-      Console.WriteLine("Sum = {0:N4}, Mean = {1:N4}, n = {2:N0}", sum, sum/calls, calls)
-      Console.WriteLine()
-      countdown.Signal()
-   End Sub
-End Class
-' The example displays output similar to the following:
-'    Thread 1 finished random number generation.
-'    Sum = 1,000,556.7483, Mean = 0.5003, n = 2,000,000
-'
-'    Thread 6 finished random number generation.
-'    Sum = 999,704.3865, Mean = 0.4999, n = 2,000,000
-'
-'    Thread 2 finished random number generation.
-'    Sum = 999,680.8904, Mean = 0.4998, n = 2,000,000
-'
-'    Thread 10 finished random number generation.
-'    Sum = 999,437.5132, Mean = 0.4997, n = 2,000,000
-'
-'    Thread 8 finished random number generation.
-'    Sum = 1,000,663.7789, Mean = 0.5003, n = 2,000,000
-'
-'    Thread 4 finished random number generation.
-'    Sum = 999,379.5978, Mean = 0.4997, n = 2,000,000
-'
-'    Thread 5 finished random number generation.
-'    Sum = 1,000,011.0605, Mean = 0.5000, n = 2,000,000
-'
-'    Thread 9 finished random number generation.
-'    Sum = 1,000,637.4556, Mean = 0.5003, n = 2,000,000
-'
-'    Thread Main finished random number generation.
-'    Sum = 1,000,676.2381, Mean = 0.5003, n = 2,000,000
-'
-'    Thread 3 finished random number generation.
-'    Sum = 999,951.1025, Mean = 0.5000, n = 2,000,000
-'
-'    Thread 7 finished random number generation.
-'    Sum = 1,000,844.5217, Mean = 0.5004, n = 2,000,000
-'
-'    22,000,000 random numbers were generated.
+        ' Simulate request processing across multiple method calls
+        PerformDatabaseOperation()
+        PerformLogging()
+    End Sub
+
+    Sub PerformDatabaseOperation()
+        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Processing DB operation for request {_requestId}")
+    End Sub
+
+    Sub PerformLogging()
+        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Logging request {_requestId}")
+    End Sub
+
+End Module
 ' </Snippet1>
