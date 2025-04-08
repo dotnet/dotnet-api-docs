9853873350a1ee402246e448a80c4804b3d250ba
Dick Baker <dickbakerm@gmail.com>
2025-04-08 04:05:43 +0100

    System.ComponentModel (#10965)

--- snippets/csharp/System.ComponentModel/LocalizableAttribute/Overview/source.cs
+++ snippets/csharp/System.ComponentModel/LocalizableAttribute/Overview/source.cs
@@ -1,32 +1,33 @@
-﻿using System;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 public class Class1
 {
     // <Snippet1>
     [Localizable(true)]
-     public int MyProperty {
-        get {
-           // Insert code here.
-           return 0;
+    public int MyProperty
+    {
+        get =>
+            // Insert code here.
+            0;
+        set
+        {
+            // Insert code here.
         }
-        set {
-           // Insert code here.
-        }
-     }
+    }
     // </Snippet1>
 
     public void Method1()
     {
         // <Snippet2>
         // Gets the attributes for the property.
-        AttributeCollection attributes = 
+        AttributeCollection attributes =
         TypeDescriptor.GetProperties(this)["MyProperty"].Attributes;
 
         // Checks to see if the property needs to be localized.
-        LocalizableAttribute myAttribute = 
+        LocalizableAttribute myAttribute =
         (LocalizableAttribute)attributes[typeof(LocalizableAttribute)];
-        if(myAttribute.IsLocalizable) {
-        // Insert code here.
+        if (myAttribute.IsLocalizable)
+        {
+            // Insert code here.
         }
         // </Snippet2>
     }
