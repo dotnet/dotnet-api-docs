6635c65afd4cbc9cd508d7a8fcbf97078e1e69bd
ManickaP <mapichov@microsoft.com>
2025-06-17 15:40:33 +0200

    Improve example for ServerCertificateValidationCallback

---
+++ snippets/csharp/System.Net.Http/WinHttpHandler/program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+class HttpClientHandler_SecureExample
+{
+    static async Task Main()
+    {
+        // <Snippet1>
+        var handler = new WinHttpHandler();
+        handler.ServerCertificateValidationCallback = (httpRequestMessage, certificate, chain, sslPolicyErrors)
+        {
+             if (sslPolicyErrors == SslPolicyErrors.None)
+                return true;
+
+            Console.WriteLine("Certificate error: {0}", sslPolicyErrors);
+
+            // Do not allow this client to communicate with unauthenticated servers.
+            return false;
+        }
+        // </Snippet1>
+    }
+}
