4deec0ab12f4c5e64b3e5423a5058bcc9c49d2cf
Marie Píchová <11718369+ManickaP@users.noreply.github.com>
2025-06-17 17:07:45 +0200

    Update snippets/csharp/System.Net.Http/WinHttpHandler/program.cs
    Co-authored-by: Anton Firszov <antonfir@gmail.com>

--- snippets/csharp/System.Net.Http/WinHttpHandler/program.cs
+++ snippets/csharp/System.Net.Http/WinHttpHandler/program.cs
@@ -4,7 +4,7 @@ using System.Net.Http;
 using System.Net.Sockets;
 using System.Threading.Tasks;
 
-class HttpClientHandler_SecureExample
+class WinHttpHandler_SecureExample
 {
     static async Task Main()
     {
