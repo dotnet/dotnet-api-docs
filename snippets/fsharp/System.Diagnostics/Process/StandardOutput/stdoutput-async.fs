270af72f1cb94b004b1001c8965417d52b6f3029
Henrique Feliciano de Azevedo <hfazevedo@furb.br>
2025-06-25 22:54:51 -0300

    Fix system.diagnostics.process.standardoutput code snippets

--- snippets/fsharp/System.Diagnostics/Process/StandardOutput/stdoutput-async.fs
+++ snippets/fsharp/System.Diagnostics/Process/StandardOutput/stdoutput-async.fs
@@ -21,8 +21,8 @@ printfn $"The last 50 characters in the output stream are:\n'{output.Substring(o
 printfn $"\nError stream: {eOut}"
 // The example displays the following output:
 //      The last 50 characters in the output stream are:
-//      ' 49,800.20%
-//      Line 500 of 500 written: 49,900.20%
+//      'ritten: 99,80%
+//      Line 500 of 500 written: 100,00%
 //      '
 //
 //      Error stream: Successfully wrote 500 lines.
--- snippets/fsharp/System.Diagnostics/Process/StandardOutput/write500lines.fs
+++ snippets/fsharp/System.Diagnostics/Process/StandardOutput/write500lines.fs
@@ -5,9 +5,11 @@ for i in 1. .. 500. do
 
 eprintfn "Successfully wrote 500 lines.";
 // The example displays the following output:
-//      The last 50 characters in the output stream are:
-//      ' 49,800.20%
-//      Line 500 of 500 written: 49,900.20%
-//'
-//
-//      Error stream: Successfully wrote 500 lines.
+//      Line 1 of 500 written: 0,20%
+//      Line 2 of 500 written: 0,40%
+//      Line 3 of 500 written: 0,60%
+//      ...
+//      Line 498 of 500 written: 99,60%
+//      Line 499 of 500 written: 99,80%
+//      Line 500 of 500 written: 100,00%
+//      Successfully wrote 500 lines.
