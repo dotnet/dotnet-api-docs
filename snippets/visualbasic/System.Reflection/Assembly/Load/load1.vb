7d4d432fea846bfa2c3facad778d1db06a198610
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2025-07-29 21:51:58 -0700

    Reorganize VB snippets in CLR directory (#11544)

--- snippets/visualbasic/System.Reflection/Assembly/Load/load1.vb
+++ snippets/visualbasic/System.Reflection/Assembly/Load/load1.vb
@@ -1,20 +1,16 @@
-﻿' Visual Basic .NET Document
-Option Strict On
-
-' <Snippet1>
+﻿' <Snippet1>
 Imports System.Reflection
 
-Module Example
-   Public Sub Main()
-      Dim longName As String = "system, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089"
-      Dim assem As Assembly = Assembly.Load(longName)
-      If assem Is Nothing Then
-         Console.WriteLine("Unable to load assembly...")
-      Else
-         Console.WriteLine(assem.FullName)
-      End If
-   End Sub
-End Module
-' The example displays the following output:
-'       system, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089
+Class Class1
+    Public Shared Sub Main()
+		' You must supply a valid fully qualified assembly name.
+		Dim SampleAssembly As [Assembly] = _
+			[Assembly].Load("SampleAssembly, Version=1.0.2004.0, Culture=neutral, PublicKeyToken=8744b20f8da049e3")
+        Dim oType As Type
+        ' Display all the types contained in the specified assembly.
+		For Each oType In SampleAssembly.GetTypes()
+			Console.WriteLine(oType.Name)
+		Next oType
+	End Sub	'LoadSample
+End Class
 ' </Snippet1>
--- snippets/visualbasic/System/String/Join/join2.vb
+++ snippets/visualbasic/System/String/Join/join2.vb
@@ -1,38 +1,19 @@
-﻿' Visual Basic .NET Document
-Option Strict On
+﻿'<snippet1>
+Class Sample
+   Public Shared Sub Main()
+      Dim val As [String]() =  {"apple", "orange", "grape", "pear"}
+      Dim sep As [String] = ", "
+      Dim result As [String]
 
-' <Snippet2>
-Imports System.Collections.Generic
-
-Module Example
-   Public Sub Main()
-      Dim maxPrime As Integer = 100
-      Dim primes As List(Of Integer) = GetPrimes(maxPrime)
-      Console.WriteLine("Primes less than {0}:", maxPrime)
-      Console.WriteLine("   {0}", String.Join(" ", primes))
+      Console.WriteLine("sep = '{0}'", sep)
+      Console.WriteLine("val() = {{'{0}' '{1}' '{2}' '{3}'}}", val(0), val(1), val(2), val(3))
+      result = [String].Join(sep, val, 1, 2)
+      Console.WriteLine("String.Join(sep, val, 1, 2) = '{0}'", result)
    End Sub
-   
-   Private Function GetPrimes(maxPrime As Integer) As List(Of Integer)
-      Dim values As Array = Array.CreateInstance(GetType(Integer), _
-                              New Integer() { maxPrime - 1}, New Integer(){ 2 }) 
-        ' Use Sieve of Eratosthenes to determine prime numbers.
-      For ctr As Integer = values.GetLowerBound(0) To _
-                           CInt(Math.Ceiling(Math.Sqrt(values.GetUpperBound(0))))
-         If CInt(values.GetValue(ctr)) = 1 Then Continue For
-         
-         For multiplier As Integer = ctr To maxPrime \ 2
-            If ctr * multiplier <= maxPrime Then values.SetValue(1, ctr * multiplier)
-         Next   
-      Next      
-      
-      Dim primes As New System.Collections.Generic.List(Of Integer)
-      For ctr As Integer = values.GetLowerBound(0) To values.GetUpperBound(0)
-         If CInt(values.GetValue(ctr)) = 0 Then primes.Add(ctr)
-      Next            
-      Return primes
-   End Function   
-End Module
-' The example displays the following output:
-'    Primes less than 100:
-'       2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97
-' </Snippet2>
+End Class
+
+'This example displays the following output:
+'       sep = ', '
+'       val() = {'apple' 'orange' 'grape' 'pear'}
+'       String.Join(sep, val, 1, 2) = 'orange, grape'
+'</snippet1>
