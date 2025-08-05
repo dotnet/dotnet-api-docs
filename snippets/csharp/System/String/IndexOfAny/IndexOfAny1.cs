85c4e3b2eb6ff6ed31058b8ca3332d019b2f94ed
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2025-08-05 06:31:24 -0700

    respond to feedback (#11649)

--- snippets/csharp/System/String/IndexOfAny/IndexOfAny1.cs
+++ snippets/csharp/System/String/IndexOfAny/IndexOfAny1.cs
@@ -8,12 +8,15 @@ public class Example1
       char[] chars = { 'a', 'e', 'i', 'o', 'u', 'y',
                        'A', 'E', 'I', 'O', 'U', 'Y' };
       String s = "The long and winding road...";
-      Console.WriteLine($"The first vowel in \n   {s}\n" +
-          $"is found at index {s.IndexOfAny(chars)}");
+      Console.WriteLine($"""
+         The first vowel in
+         '{s}'
+         is found at index {s.IndexOfAny(chars)}
+         """);
 
       // The example displays the following output:
       //       The first vowel in
-      //          The long and winding road...
+      //       'The long and winding road...'
       //       is found at index 2
       // </Snippet1>
    }
