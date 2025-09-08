f9f1a1d2334c209da03f7b89fda548c9af347ef9
Stuart Mosquera <stuartmosquera18@gmail.com>
2025-09-08 12:31:56 -0300

    add missing examples for EnumerateFiles

---
+++ snippets/visualbasic/System.IO/Directory/EnumerateFiles/program2.vb
@@ -0,0 +1,39 @@
+' <Snippet1>
+Imports System.IO
+Imports System.Xml.Linq
+
+Module Module1
+    Sub Main()
+        Try
+            ' Set a variable to the My Documents Path.
+            Dim docPath As String = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)
+
+            ' Set the options for the enumeration.
+            Dim options As New EnumerationOptions() With {
+                .IgnoreInaccessible = True,
+                .MatchCasing = MatchCasing.CaseInsensitive,
+                .MatchType = MatchType.Simple,
+                .RecurseSubdirectories = True
+            }
+
+            Dim files = From chkFile In Directory.EnumerateFiles(docPath, "*.txt", options)
+                        From line In File.ReadLines(chkFile)
+                        Where line.Contains("Microsoft")
+                        Select New With {
+                            .curFile = chkFile,
+                            .curLine = line
+                        }
+
+            For Each f In files
+                Console.WriteLine($"{f.curFile}\t{f.curLine}")
+            Next
+
+            Console.WriteLine($"{files.Count} files found.")
+        Catch pathEx As PathTooLongException
+            Console.WriteLine(pathEx.Message)
+        Catch ex As Exception
+            Console.WriteLine(ex.Message)
+        End Try
+    End Sub
+End Module
+' </Snippet1>
