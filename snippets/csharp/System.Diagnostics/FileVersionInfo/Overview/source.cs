dfa02549388f3e80168c057f117a3170f40f79e8
Connor O'Shea <connor@connoroshea.com>
2025-10-27 12:52:23 -0400

    Use Path.Combine and Environment.NewLine

--- snippets/csharp/System.Diagnostics/FileVersionInfo/Overview/source.cs
+++ snippets/csharp/System.Diagnostics/FileVersionInfo/Overview/source.cs
@@ -11,10 +11,10 @@ class Class1
         // Get the file version for the notepad.
         // Use either of the two following commands.
         FileVersionInfo.GetVersionInfo(Path.Combine(Environment.SystemDirectory, "Notepad.exe"));
-        FileVersionInfo myFileVersionInfo = FileVersionInfo.GetVersionInfo(Environment.SystemDirectory + "\\Notepad.exe");
+        FileVersionInfo myFileVersionInfo = FileVersionInfo.GetVersionInfo(Path.Combine(Environment.SystemDirectory, "Notepad.exe"));
 
         // Print the file name and version number.
-        Console.WriteLine("File: " + myFileVersionInfo.FileDescription + '\n' +
+        Console.WriteLine("File: " + myFileVersionInfo.FileDescription + Environment.NewLine +
            "Version number: " + myFileVersionInfo.FileVersion);
     }
 }
