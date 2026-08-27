f23b5177ffd48b575edc51630208195e214371f9
Genevieve Warren <24882762+gewarren@users.noreply.github.com>
2026-08-27 13:28:29 -0700

    reset some changes

--- snippets/csharp/System.Diagnostics/Process/Overview/processstartstatic2.cs
+++ snippets/csharp/System.Diagnostics/Process/Overview/processstartstatic2.cs
@@ -1,6 +1,6 @@
-﻿// This sample starts the argument-echo helper in the same snippet project.
+﻿// Place this code into a console project called StartArgsEcho. It depends on the
+// console application named argsecho.exe.
 
-using System;
 using System.Diagnostics;
 
 namespace StartArgsEcho
@@ -9,14 +9,14 @@ namespace StartArgsEcho
     {
         public static void Run()
         {
-            ProcessStartInfo startInfo = new(SampleProcess.FileName)
+            ProcessStartInfo startInfo = new("argsecho.exe")
             {
                 WindowStyle = ProcessWindowStyle.Normal,
 
                 // Start with one argument.
                 // Output of ArgsEcho:
                 //  [0]=/a
-                Arguments = SampleProcess.Arguments("/a")
+                Arguments = "/a"
             };
             Process.Start(startInfo);
 
@@ -25,14 +25,14 @@ namespace StartArgsEcho
             //  [0] = /a
             //  [1] = /b
             //  [2] = c:\temp
-            startInfo.Arguments = SampleProcess.Arguments("/a /b c:\\temp");
+            startInfo.Arguments = "/a /b c:\\temp";
             Process.Start(startInfo);
 
             // An argument with spaces inside quotes is interpreted as multiple arguments.
             // Output of ArgsEcho:
             //  [0] = /a
             //  [1] = literal string arg
-            startInfo.Arguments = SampleProcess.Arguments("/a \"literal string arg\"");
+            startInfo.Arguments = "/a \"literal string arg\"";
             Process.Start(startInfo);
 
             // An argument inside double quotes is interpreted as if the quote weren't there,
@@ -43,14 +43,14 @@ namespace StartArgsEcho
             //  [2] = in
             //  [3] = double
             //  [4] = quotes
-            startInfo.Arguments = SampleProcess.Arguments("/a /b:\"\"string in double quotes\"\"");
+            startInfo.Arguments = "/a /b:\"\"string in double quotes\"\"";
             Process.Start(startInfo);
 
             // Triple-escape quotation marks to include the character in the final argument received
             // by the target process. Equivalent verbatim string: @"/a /b:""""""quoted string""""""";
             //  [0] = /a
             //  [1] = /b:"quoted string"
-            startInfo.Arguments = SampleProcess.Arguments("/a /b:\"\"\"quoted string\"\"\"");
+            startInfo.Arguments = "/a /b:\"\"\"quoted string\"\"\"";
             Process.Start(startInfo);
         }
     }
